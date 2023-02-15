8b04816c7593987a22dfb441f9cb9a72ce3dfcd8
aeong98 <sy9815@gmail.com>
2023-02-15 22:17:36 +0900

    예제 코드 추가

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,30 @@
 import CheckBox from '@/common-ui/CheckBox';
+import styled from '@emotion/styled';
 
 const Test = () => {
   const onChange = (checked: boolean) => {
     console.log(checked);
   };
-  return <CheckBox onChange={onChange}>hi</CheckBox>;
+  return (
+    <Container>
+      <CheckBox onChange={onChange}>
+        <Text>hi</Text>
+      </CheckBox>
+      <CheckBox onChange={onChange} isChecked={true}>
+        <Text>hi</Text>
+      </CheckBox>
+    </Container>
+  );
 };
 
 export default Test;
+
+const Container = styled.div`
+  width: 200px;
+  height: fit-content;
+  background-color: ${(p) => p.theme.colors.black};
+`;
+
+const Text = styled.span`
+  color: ${(p) => p.theme.colors.white};
+`;
