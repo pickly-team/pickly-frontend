d557c7646d51c37f52be38e017cfec89a4be332f
aeong98 <sy9815@gmail.com>
2023-02-15 22:12:05 +0900

    theme.ts > 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: '#76B100',
+    lightPrimary: '#A7E100',
+    darkPrimary: '#8EC520',
+    white: '#ffffff',
+    black: '#212121',
+    grey: '#757575',
+    lightGrey: '#EEEEEE',
+    darkGrey: '#424242',
+    mediumGrey: '#BDBDBD',
+  },
+};
