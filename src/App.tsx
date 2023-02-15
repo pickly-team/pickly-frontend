e3ebf16e944a55609f0c3e0d12dad50d2b37224c
aeong98 <sy9815@gmail.com>
2023-02-15 22:12:05 +0900

    theme.ts > 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Test from '@/components/Test';
 import Bear from '@/components/Bear';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '@/styles/theme';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={'/'} element={<Test />} />
-          <Route path={'/bear'} element={<Bear />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<Test />} />
+            <Route path={'/bear'} element={<Bear />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
