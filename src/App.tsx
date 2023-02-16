5c600900ca59a6914705ad17ef33e7f23dd5babb
ww8007 <wshmin1234@gmail.com>
2023-02-16 23:30:59 +0900

    feat: 전역 IconLoader, GlobalStyle 로드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Bear from '@/components/Bear';
+import { theme } from '@/styles/theme';
+import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Bear from '@/components/Bear';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from '@/styles/theme';
+import IconLoader from './common-ui/assets/IconLoader';
+import GlobalStyle from './styles/GlobalStyle';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +13,8 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
+          <IconLoader />
+          <GlobalStyle />
           <Routes>
             <Route path={'/'} element={<Bear />} />
           </Routes>
