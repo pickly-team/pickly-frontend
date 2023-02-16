ba1a6b5ea171df2f667b1f654ae7686971b990b8
ww8007 <wshmin1234@gmail.com>
2023-02-16 23:46:17 +0900

    feat: svg Icon load 컴포넌트 추가 1. IconLoader에 svg 아이콘 추가 2. IconName에 아이콘 명 추가

---
+++ src/common-ui/assets/Icon.tsx
@@ -0,0 +1,34 @@
+// TODO : 아이콘 사이즈 논의 필요
+const sizeMap = {
+  l: 28,
+  m: 24,
+  s: 20,
+  xs: 15,
+} as const;
+
+interface IconProps {
+  name: IconName;
+  size: keyof typeof sizeMap;
+}
+
+const Icon = ({ name, size }: IconProps) => (
+  <svg
+    width={sizeMap[size]}
+    height={sizeMap[size]}
+    fill="none"
+    style={{ pointerEvents: 'none' }}
+  >
+    <use href={`#${name}`} />
+  </svg>
+);
+
+export default Icon;
+
+export type IconName =
+  | 'check'
+  | 'check-none'
+  | 'like'
+  | 'like-green'
+  | 'message'
+  | 'message-on-green'
+  | 'not-read';
