7daed2679366665c5b0d4cb13c5939f5dd96b15a
ww8007 <wshmin1234@gmail.com>
2023-02-25 23:36:56 +0900

    feat: Input 공통 UI 컴포넌트 추가

---
+++ src/common-ui/Input.test.tsx
@@ -0,0 +1,137 @@
+import { render } from '@/test/utils';
+import { fireEvent, screen } from '@testing-library/react';
+import { useState } from 'react';
+import Input, { InputProps } from './Input';
+
+describe('Input Test', () => {
+  it('Input 컴포넌트는 type에 따라 input 태그를 렌더링한다.', () => {
+    //given
+    const InputComponent = () => {
+      const [value, setValue] = useState('');
+      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+      };
+      return <Input onChange={onChange} value={value} />;
+    };
+
+    //when
+    render(<InputComponent />);
+    //then
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveStyle('border: none');
+  });
+  it('Input 컴포넌트 입력 테스트', () => {
+    //given
+    const InputComponent = () => {
+      const [value, setValue] = useState('');
+      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+      };
+      return (
+        <Input
+          onChange={onChange}
+          value={value}
+          placeholderText="입력 테스트"
+        />
+      );
+    };
+
+    //when
+    render(<InputComponent />);
+
+    //then
+    const input = screen.getByRole('textbox');
+
+    fireEvent.change(input, { target: { value: 'Hello' } });
+    expect(input).toHaveAttribute('value', 'Hello');
+  });
+  it('Input border 테스트', () => {
+    //given
+    const InputComponent = () => {
+      const [value, setValue] = useState('안녕하세요');
+      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+      };
+      return (
+        <Input
+          onChange={onChange}
+          border={{ borderRadius: 10, borderWidth: 3, color: 'grey900' }}
+          placeholderText="입력 테스트"
+          value={value}
+        />
+      );
+    };
+
+    //when
+    render(<InputComponent />);
+
+    //then
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveStyle('border: 3px solid #212121 ');
+    expect(input).toHaveStyle('border-radius: 10px');
+  });
+  it('Input focus 테스트', () => {
+    //given
+    const placeholderText = '입력 테스트';
+
+    const InputComponent = () => {
+      const [value, setValue] = useState('안녕하세요');
+      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+      };
+      return (
+        <Input
+          onChange={onChange}
+          placeholderText={placeholderText}
+          value={value}
+        />
+      );
+    };
+
+    //when
+    render(<InputComponent />);
+
+    //then
+    const input = screen.getByPlaceholderText(placeholderText);
+    input.focus();
+
+    expect(input).toHaveFocus(); // throws
+    expect(input).toHaveStyle('color: #ffffff');
+    expect(input).toHaveStyle('background-color: #424242');
+  });
+  it('Input focus 테스트 > focus Props 존재', () => {
+    //given
+    const placeholderText = '입력 테스트';
+    const focus: Pick<InputProps, 'focusTheme'> = {
+      focusTheme: {
+        color: 'primary',
+        backgroundColor: 'grey900',
+      },
+    };
+    const InputComponent = () => {
+      const [value, setValue] = useState('안녕하세요');
+      const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+      };
+      return (
+        <Input
+          onChange={onChange}
+          focusTheme={focus.focusTheme}
+          placeholderText={placeholderText}
+          value={value}
+        />
+      );
+    };
+
+    //when
+    render(<InputComponent />);
+
+    //then
+    const input = screen.getByPlaceholderText(placeholderText);
+    input.focus();
+    expect(input).toHaveFocus();
+    expect(input).toHaveStyle('color: #76B100');
+    expect(input).toHaveStyle('background-color: #212121');
+  });
+});
---
+++ src/common-ui/Input.tsx
@@ -0,0 +1,87 @@
+import { theme } from '@/styles/theme';
+import { css } from '@emotion/react';
+import {
+  ChangeEvent,
+  ComponentPropsWithRef,
+  forwardRef,
+  ForwardRefRenderFunction,
+} from 'react';
+
+export type InputProps = {
+  value: string;
+  color?: keyof typeof theme.colors;
+  backgroundColor?: keyof typeof theme.colors;
+  focusTheme?: {
+    color?: keyof typeof theme.colors;
+    backgroundColor?: keyof typeof theme.colors;
+  };
+  border?: {
+    color?: keyof typeof theme.colors;
+    borderWidth?: number;
+    borderRadius?: number;
+  };
+  placeholderText?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+} & ComponentPropsWithRef<'input'>;
+
+const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  {
+    color = 'white',
+    backgroundColor = 'grey900',
+    border = {
+      borderRadius: 10,
+      borderWidth: 0,
+      color: undefined,
+    },
+    focusTheme = {
+      color: 'white',
+      backgroundColor: 'grey800',
+    },
+    placeholderText = '',
+    ...restProps
+  },
+  ref,
+) => {
+  return (
+    <input
+      type={'text'}
+      placeholder={placeholderText}
+      css={css`
+        box-shadow: none;
+        font-size: 1rem;
+        appearance: none;
+        color: ${theme.colors[color]};
+        font-family: 'NanumSquareRoundR';
+        border-radius: ${border.borderRadius}px;
+        border: ${border.borderWidth && border.color
+          ? `${border.borderWidth}px solid ${theme.colors[border.color]}`
+          : 'none'};
+        background-color: ${theme.colors[backgroundColor]};
+        transition: all ease 0.5s 0s;
+        width: 100%;
+        height: 2.5rem;
+        padding: 1.5rem;
+        box-sizing: border-box;
+        :focus {
+          outline: none;
+          color: ${focusTheme.color
+            ? theme.colors[focusTheme.color]
+            : theme.colors.white};
+          background-color: ${focusTheme.backgroundColor
+            ? theme.colors[focusTheme.backgroundColor]
+            : theme.colors.grey800};
+        }
+        ::placeholder {
+          color: ${theme.colors.black};
+        }
+        :disabled {
+          background-color: ${theme.colors.grey200};
+        }
+      `}
+      ref={ref}
+      {...restProps}
+    />
+  );
+};
+
+export default forwardRef(Input);
