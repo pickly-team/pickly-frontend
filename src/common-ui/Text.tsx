db527b88a6baa430e0c53a2328c73fe53cbab29a
ww8007 <wshmin1234@gmail.com>
2023-02-25 23:36:30 +0900

    fix: 동일하던 type과 tag의 목적성 분리 content | header

--- src/common-ui/Text.tsx
+++ src/common-ui/Text.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { theme } from '@/styles/theme';
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
 import { FunctionComponent } from 'react';
 
 type StrictPropsWithChildren<P = unknown> = P & { children: string };
@@ -11,31 +11,35 @@ type HeaderText = {
 };
 
 type SpanText = {
-  type: 'span';
+  type: 'content';
   tag: 'span';
 };
 
 type LiText = {
-  type: 'div';
+  type: 'content';
   tag: 'div';
 };
 
 type PText = {
-  type: 'p';
+  type: 'content';
   tag: 'p';
 };
 
+export type HeaderType = HeaderText | SpanText | LiText | PText;
+
 interface TextProps extends StrictPropsWithChildren {
-  type: HeaderText | SpanText | LiText | PText;
-  weight: 'bold' | 'regular';
+  type: HeaderType;
+  weight?: 'bold' | 'regular';
   fontSize?: number;
   color?: keyof typeof theme.colors;
+  customCss?: SerializedStyles;
 }
 
 const cssText = ({
   weight,
   color,
   fontSize,
+  customCss,
 }: Omit<TextProps, 'type' | 'children' | 'color'> & {
   color: keyof typeof theme.colors;
 }) =>
@@ -45,12 +49,13 @@ const cssText = ({
     font-family: ${weight === 'bold'
       ? 'NanumSquareRoundB'
       : 'NanumSquareRoundR'};
+    ${customCss}
   `;
 
 const HeaderNode: Record<
   HeaderText['tag'],
   FunctionComponent<
-    Omit<TextProps, 'type'> & { color: keyof typeof theme.colors }
+    Omit<TextProps, 'type'> & { color: keyof typeof theme.colors | 'inherit' }
   >
 > = {
   h1: ({ children, color, fontSize, weight }) => (
@@ -66,6 +71,11 @@ const HeaderNode: Record<
 
 /**
  *
+ * @param type - 태그와 헤더 여부를 지정
+ * @param weight - 폰트 굵기
+ * @param color - 폰트 색상
+ * @param fontSize - 폰트 크기
+ * @param customCss - 추가적인 css
  * @example
  *  <Text type={{ tag: 'h3', type: 'header' }} weight="bold" color="white">
         이것은 텍스트 컴포넌트 입니다
@@ -74,17 +84,22 @@ const HeaderNode: Record<
 
 const Text: FunctionComponent<TextProps> = ({
   type,
-  weight,
-  fontSize = 24,
+  weight = 'regular',
+  fontSize = 1,
   color = 'white',
   children,
+  customCss,
 }) => {
   if (type.type === 'header') {
-    return <>{HeaderNode[type.tag]({ children, color, fontSize, weight })}</>;
+    return (
+      <>
+        {HeaderNode[type.tag]({ children, color, fontSize, weight, customCss })}
+      </>
+    );
   } else {
     return (
       <>
-        <type.tag css={cssText({ color, fontSize, weight })}>
+        <type.tag css={cssText({ color, fontSize, weight, customCss })}>
           {children}
         </type.tag>
       </>
