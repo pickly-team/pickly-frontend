5c52310c84f6dc85a51dd3c7dab5bdb761ee3dde
aeong98 <sy9815@gmail.com>
2023-02-26 14:24:47 +0900

    test: mouse,touch 이벤트 추가

--- src/common-ui/SlideItem.test.tsx
+++ src/common-ui/SlideItem.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@/test/utils';
 import SlideItem from '@/common-ui/SlideItem';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 describe('SlideItem test', () => {
   test('main 엘리먼트가 렌더링 된다.', () => {
@@ -24,4 +24,34 @@ describe('SlideItem test', () => {
     screen.getByText(OPTION);
   });
   //TODO: cypress로 슬라이드 테스트 추가
+  test('mouse event', () => {
+    //given
+    const MAIN = '슬라이드 아이템';
+    const OPTION = '옵션';
+
+    //when
+    render(<SlideItem main={<div>{MAIN}</div>} option={<div>{OPTION}</div>} />);
+    const element = screen.getByText(MAIN);
+    fireEvent.mouseDown(element);
+    fireEvent.mouseMove(element, { clientX: -100 });
+    fireEvent.mouseUp(element);
+
+    //then
+    screen.getByText(OPTION);
+  });
+  test('touch event', () => {
+    //given
+    const MAIN = '슬라이드 아이템';
+    const OPTION = '옵션';
+
+    //when
+    render(<SlideItem main={<div>{MAIN}</div>} option={<div>{OPTION}</div>} />);
+    const element = screen.getByText(MAIN);
+    fireEvent.touchStart(element);
+    fireEvent.touchMove(element, { clientX: -100 });
+    fireEvent.touchEnd(element);
+
+    //then
+    screen.getByText(OPTION);
+  });
 });
