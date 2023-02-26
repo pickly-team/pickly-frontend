7135202c544b603a9a920e1cc25edbf47c4f83ef
aeong98 <sy9815@gmail.com>
2023-02-26 14:46:54 +0900

    fix: lint 에러 fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,34 +3,58 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true
+    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:jest/recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier', 'unused-imports', 'jest'],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+    'unused-imports',
+    'jest',
+  ],
   rules: {
     'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
     'react/react-in-jsx-scope': 'off',
-    'react/function-component-definition': [2, {
-      namedComponents: 'arrow-function'
-    }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
     quotes: ['error', 'single'],
     'react-hooks/rules-of-hooks': 'error',
-    'jest/expect-expect': ['off', {
-      assertFunctionNames: ['expect', 'fc.assert']
-    }],
+    'jest/expect-expect': [
+      'off',
+      {
+        assertFunctionNames: ['expect', 'fc.assert'],
+      },
+    ],
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-empty-function': 'warn',
-    'react/no-unknown-property': ['error', {
-      ignore: ['css']
-    }]
-  }
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['css'],
+      },
+    ],
+  },
 };
