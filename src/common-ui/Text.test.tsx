ca5622fd32d83c10ed9f978512c714b28071bffb
ww8007 <wshmin1234@gmail.com>
2023-03-03 01:40:49 +0900

    refactor: Text.Div와 같은 형태로 간소화 header: h1, h2, h3 span div p

--- src/common-ui/Text.test.tsx
+++ src/common-ui/Text.test.tsx
@@ -1,132 +1,78 @@
 import { render, screen } from '@testing-library/react';
-import Text, { HeaderType } from './Text';
+import Text from './Text';
 
 describe('Text test', () => {
-  test('Text 컴포넌트는 children을 렌더링한다.', () => {
+  test('Text 컴포넌트는 children(string)을 렌더링한다.', () => {
     //given
     const TEXT = 'Hello World';
 
     //when
-    render(
-      <Text type={{ tag: 'h1', type: 'header' }} weight="bold">
-        {TEXT}
-      </Text>,
-    );
+    render(<Text.Div>{TEXT}</Text.Div>);
 
     //then
     expect(screen.getByText(TEXT)).toBeInTheDocument();
   });
-  test('Text 컴포넌트는 type에 따라 태그를 렌더링한다.', () => {
-    //given
-    const TYPE: HeaderType = { tag: 'h2', type: 'header' };
-
-    //when
-    render(
-      <Text type={TYPE} weight="bold">
-        TEXT
-      </Text>,
-    );
+  test('Text 컴포넌트 > Header 렌더링 테스트', () => {
+    //given && when
+    const TEXT = 'Hello World';
+    render(<Text.Header level="h1">{TEXT}</Text.Header>);
 
     //then
-    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
   });
-  test('Text 컴포넌트는 weight에 따라 폰트를 렌더링한다. Bold', () => {
+  test('Text 컴포넌트 > Span 렌더링 테스트', () => {
+    //given && when
+    const TEXT = 'Hello World';
+    render(<Text.Span>{TEXT}</Text.Span>);
+
+    //then
+    // span을 특정할 수 없어서 다음과 같이 테스트
+    expect(screen.getByText(TEXT)).toBeInTheDocument();
+  });
+  test('Text 컴포넌트 > P 렌더링 테스트', () => {
+    //given && when
+    const TEXT = 'Hello World';
+    render(<Text.P>{TEXT}</Text.P>);
+
+    //then
+    // span을 특정할 수 없어서 다음과 같이 테스트
+    // expect(screen.getByRole('paragraph')).toBeInTheDocument();
+    expect(screen.getByText(TEXT)).toBeInTheDocument();
+  });
+  test('Text 컴포넌트는 weight에 따라 폰트를 렌더링한다.', () => {
     //given
     const weight = 'bold';
-    const TEXT = 'Hello World';
 
     //when
-    render(
-      <Text type={{ tag: 'h2', type: 'header' }} weight={weight}>
-        {TEXT}
-      </Text>,
-    );
+    render(<Text.Div weight={weight}>TEXT</Text.Div>);
 
     //then
-    expect(screen.getByText(TEXT)).toHaveStyle(
-      'font-family: NanumSquareRoundB',
-    );
+    expect(screen.getByText('TEXT')).toHaveStyle({
+      'font-family': 'NanumSquareRoundB',
+    });
   });
-  test('Text 컴포넌트는 weight에 따라 폰트를 렌더링한다. Regular', () => {
-    //given
-    const TEXT = 'Hello World';
-
-    //when
-    render(
-      <Text type={{ tag: 'h2', type: 'header' }} weight="regular">
-        {TEXT}
-      </Text>,
-    );
-
-    //then
-    expect(screen.getByText(TEXT)).toHaveStyle(
-      'font-family: NanumSquareRoundR',
-    );
-  });
-
   test('Text 컴포넌트는 fontSize에 따라 폰트 사이즈를 렌더링한다.', () => {
     //given
-    const TEXT = 'Hello World';
-    const FONTSIZE = 2;
+    const fontSize = 2;
 
     //when
-    render(
-      <Text
-        type={{ tag: 'h2', type: 'header' }}
-        weight="bold"
-        fontSize={FONTSIZE}
-      >
-        {TEXT}
-      </Text>,
-    );
+    render(<Text.Div fontSize={fontSize}>TEXT</Text.Div>);
 
     //then
-    expect(screen.getByText(TEXT)).toHaveStyle('font-size: 2rem');
+    expect(screen.getByText('TEXT')).toHaveStyle({
+      'font-size': '2rem',
+    });
   });
   test('Text 컴포넌트는 color에 따라 폰트 색상을 렌더링한다.', () => {
     //given
-    const TEXT = 'Hello World';
-    const COLOR = 'black';
+    const color = 'grey900';
 
     //when
-    render(
-      <Text type={{ tag: 'h3', type: 'header' }} weight="bold" color={COLOR}>
-        {TEXT}
-      </Text>,
-    );
+    render(<Text.Div color={color}>TEXT</Text.Div>);
 
     //then
-    expect(screen.getByText(TEXT)).toHaveStyle('color: #161617');
-  });
-
-  test('Text 컴포넌트는 선언한 tag에 따라 텍스트를 렌더링한다.', () => {
-    //given
-    const TEXT = 'Hello World';
-    const TYPE: HeaderType = { tag: 'span', type: 'content' };
-
-    //when
-    render(
-      <Text type={TYPE} weight="bold">
-        {TEXT}
-      </Text>,
-    );
-
-    //then
-    const spanText = document.querySelector('span');
-    expect(spanText).toBeInTheDocument();
-  });
-  test('지정된 기본 color 색상이 없다면 흰색을 보여준다.', () => {
-    //given
-    const TEXT = 'Hello World';
-
-    //when
-    render(
-      <Text type={{ tag: 'span', type: 'content' }} weight="bold">
-        {TEXT}
-      </Text>,
-    );
-
-    //then
-    expect(screen.getByText(TEXT)).toHaveStyle('color: #ffffff');
+    expect(screen.getByText('TEXT')).toHaveStyle({
+      color: '#212121',
+    });
   });
 });
--- src/common-ui/Text.tsx
+++ src/common-ui/Text.tsx
@@ -1,110 +1,95 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { theme } from '@/styles/theme';
-import { css, SerializedStyles } from '@emotion/react';
-import { FunctionComponent } from 'react';
+import { ColorType, theme } from '@/styles/theme';
+import { css } from '@emotion/react';
+import type { FunctionComponent, HTMLAttributes } from 'react';
 
 type StrictPropsWithChildren<P = unknown> = P & { children: string };
 
-type HeaderText = {
-  type: 'header';
-  tag: 'h1' | 'h2' | 'h3';
-};
-
-type SpanText = {
-  type: 'content';
-  tag: 'span';
-};
-
-type LiText = {
-  type: 'content';
-  tag: 'div';
-};
-
-type PText = {
-  type: 'content';
-  tag: 'p';
-};
-
-export type HeaderType = HeaderText | SpanText | LiText | PText;
-
-interface TextProps extends StrictPropsWithChildren {
-  type: HeaderType;
+type TextProps = {
   weight?: 'bold' | 'regular';
   fontSize?: number;
-  color?: keyof typeof theme.colors;
-  customCss?: SerializedStyles;
+  color?: ColorType;
+} & StrictPropsWithChildren &
+  HTMLAttributes<HTMLParagraphElement | HTMLSpanElement | HTMLDivElement>;
+
+type HeaderType = 'h1' | 'h2' | 'h3';
+
+interface HeaderTextProps extends TextProps {
+  level: HeaderType;
 }
+
+/**
+ * 
+ * @param fontSize - rem 단위
+ * @example
+ *  <Text.div fontSize={1} weight="bold">
+        안녕하세요 예시 테스트 div 입니다.
+    </Text.div>
+ */
 
 const cssText = ({
   weight,
-  color,
+  color = 'white',
   fontSize,
-  customCss,
-}: Omit<TextProps, 'type' | 'children' | 'color'> & {
-  color: keyof typeof theme.colors;
-}) =>
+}: Omit<TextProps, 'children'>) =>
   css`
     font-size: ${fontSize}rem;
     color: ${theme.colors[color]};
     font-family: ${weight === 'bold'
       ? 'NanumSquareRoundB'
       : 'NanumSquareRoundR'};
-    ${customCss}
   `;
 
-const HeaderNode: Record<
-  HeaderText['tag'],
-  FunctionComponent<
-    Omit<TextProps, 'type'> & { color: keyof typeof theme.colors | 'inherit' }
-  >
-> = {
-  h1: ({ children, color, fontSize, weight }) => (
-    <h2 css={cssText({ color, fontSize, weight })}>{children}</h2>
-  ),
-  h2: ({ children, color, fontSize, weight }) => (
-    <h2 css={cssText({ color, fontSize, weight })}>{children}</h2>
-  ),
-  h3: ({ children, color, fontSize, weight }) => (
-    <h2 css={cssText({ color, fontSize, weight })}>{children}</h2>
-  ),
-} as const;
-
-/**
- *
- * @param type - 태그와 헤더 여부를 지정
- * @param weight - 폰트 굵기
- * @param color - 폰트 색상
- * @param fontSize - 폰트 크기
- * @param customCss - 추가적인 css
- * @example
- *  <Text type={{ tag: 'h3', type: 'header' }} weight="bold" color="white">
-        이것은 텍스트 컴포넌트 입니다
-    </Text>
- */
-
-const Text: FunctionComponent<TextProps> = ({
-  type,
+const P: FunctionComponent<TextProps> = ({
   weight = 'regular',
   fontSize = 1,
-  color = 'white',
+  color,
   children,
-  customCss,
 }) => {
-  if (type.type === 'header') {
-    return (
-      <>
-        {HeaderNode[type.tag]({ children, color, fontSize, weight, customCss })}
-      </>
-    );
-  } else {
-    return (
-      <>
-        <type.tag css={cssText({ color, fontSize, weight, customCss })}>
-          {children}
-        </type.tag>
-      </>
-    );
-  }
+  return <p css={cssText({ color, fontSize, weight })}>{children}</p>;
+};
+
+const Span: FunctionComponent<TextProps> = ({
+  weight = 'regular',
+  fontSize = 1,
+  color,
+  children,
+}) => {
+  return <span css={cssText({ color, fontSize, weight })}>{children}</span>;
+};
+
+const Div: FunctionComponent<TextProps> = ({
+  weight = 'regular',
+  fontSize = 1,
+  color,
+  children,
+}) => {
+  return <span css={cssText({ color, fontSize, weight })}>{children}</span>;
+};
+
+const Header: FunctionComponent<HeaderTextProps> = ({
+  level,
+  weight = 'regular',
+  fontSize = 1,
+  color,
+  children,
+}) => {
+  const Tag = level;
+  return <Tag css={cssText({ color, fontSize, weight })}>{children}</Tag>;
+};
+
+type TextType = {
+  P: FunctionComponent<TextProps>;
+  Header: FunctionComponent<TextProps & { level: HeaderType }>;
+  Span: FunctionComponent<TextProps>;
+  Div: FunctionComponent<TextProps>;
+};
+
+const Text: TextType = {
+  P,
+  Header,
+  Span,
+  Div,
 };
 
 export default Text;
