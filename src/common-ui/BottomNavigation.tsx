7044596f529723756f744e16a62c9f8206ea868a
ww8007 <wshmin1234@gmail.com>
2023-03-05 00:04:07 +0900

    fix: navigatePath path 변경 사항 반영

--- src/common-ui/BottomNavigation.tsx
+++ src/common-ui/BottomNavigation.tsx
@@ -1,4 +1,4 @@
-import { navigatePath } from '@/config/navigatePath';
+import { navigatePath } from '@/constant/navigatePath';
 import { theme } from '@/styles/theme';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
