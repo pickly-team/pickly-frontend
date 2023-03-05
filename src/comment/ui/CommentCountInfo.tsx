438db3c07e1bb524bbb97e7c710a484bb17477ae
aeong98 <sy9815@gmail.com>
2023-03-05 16:27:55 +0900

    feat: comment count info 추가

---
+++ src/comment/ui/CommentCountInfo.tsx
@@ -0,0 +1,20 @@
+import Icon from '@/common-ui/assets/Icon';
+import Text from '@/common-ui/Text';
+import getRem from '@/utils/getRem';
+import styled from '@emotion/styled';
+
+const CommentCountInfo = ({ commentCount }: { commentCount: number }) => {
+  return (
+    <Container>
+      <Icon name="message-green" size="m" />
+      <Text.Span fontSize={getRem(16)}>{`: ${commentCount}`}</Text.Span>
+    </Container>
+  );
+};
+
+export default CommentCountInfo;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
