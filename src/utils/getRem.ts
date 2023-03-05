d1863ef3befd60c34cb3c1c8246f7720d2882ba8
aeong98 <sy9815@gmail.com>
2023-03-05 21:16:53 +0900

    fix: getRem number[] 도 받을수있도록 수정

--- src/utils/getRem.ts
+++ src/utils/getRem.ts
@@ -1,5 +1,13 @@
-function getRem(pixels: number): string {
-  return `${0.0625 * pixels}rem`;
+function getRem(pixels: number | number[]): string {
+  if (typeof pixels === 'number') {
+    return `${calculateRem(pixels)}rem`;
+  } else {
+    return pixels.map((pixel) => `${calculateRem(pixel)}rem`).join(' ');
+  }
 }
+
+const calculateRem = (pixel: number) => {
+  return 0.0625 * pixel;
+};
 
 export default getRem;
