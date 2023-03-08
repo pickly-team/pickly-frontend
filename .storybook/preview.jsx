884769562d351fd705f48dc6dec1a1f3f154fc51
aeong98 <sy9815@gmail.com>
2023-03-08 21:24:22 +0900

    story : Select 컴포넌트 스타일북 추가

---
+++ .storybook/preview.jsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '@/styles/theme';
+import GlobalStyle from '@/styles/GlobalStyle';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators =[
+  Story => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Story/>
+    </ThemeProvider>
+  )
+]
