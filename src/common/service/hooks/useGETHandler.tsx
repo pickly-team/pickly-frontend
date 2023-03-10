442c5dd6c9a5dbc7870a5fe376e4d0bec8a88b19
ww8007 <wshmin1234@gmail.com>
2023-03-10 15:52:47 +0900

    feat: 임시 GET 요청 처리용 커스텀 훅 추가

---
+++ src/common/service/hooks/useGETHandler.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+const useGETHandler = <T,>(fetchCallback: () => Promise<T>, enabled = true) => {
+  const [data, setData] = useState<T>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<unknown>();
+  const isError = useMemo(() => !!error, [error]);
+  const refetch = useCallback(async () => {
+    if (!enabled) return;
+    try {
+      setIsLoading(true);
+      const result = await fetchCallback();
+      setData(result);
+      return result;
+    } catch (e) {
+      console.error(e);
+      setError(e);
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  }, [enabled, fetchCallback]);
+
+  useEffect(() => {
+    refetch();
+  }, [enabled]);
+  return { data, setData, isLoading, isError, error, refetch };
+};
+
+export default useGETHandler;
