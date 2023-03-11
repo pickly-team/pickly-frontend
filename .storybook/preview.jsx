6d0439cf8525791171bd3e4ad5da493917b20ea7
ww8007 <wshmin1234@gmail.com>
2023-03-12 05:53:34 +0900

    feat: 스토리북 다크 모드 및 IconLoader 추가

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from '@emotion/react';
-import { theme } from '@/styles/theme';
-import GlobalStyle from '@/styles/GlobalStyle';
+import IconLoader from '../src/common-ui/assets/IconLoader';
+import { theme } from '../src/styles/theme';
+import GlobalStyle from '../src/styles/GlobalStyle';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
 
-export const decorators =[
-  Story => (
+export const decorators = [
+  (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-      <Story/>
+      <IconLoader />
+      <GlobalStyle />
+      <Story />
     </ThemeProvider>
-  )
-]
+  ),
+];
