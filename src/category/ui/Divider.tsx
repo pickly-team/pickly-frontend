7b6aa49a2308452a2e8877904e0c92f1608ae12b
ww8007 <wshmin1234@gmail.com>
2023-03-27 01:44:12 +0900

    feat: Divider UI 추가 추후 공통으로 빼야할 것 같음

---
+++ src/category/ui/Divider.tsx
@@ -0,0 +1,46 @@
+import { theme } from '@/styles/theme';
+import styled from '@emotion/styled';
+
+type Size = 's' | 'l';
+type Margin = 'on' | 'off';
+type TypeStyle = {
+  height: number;
+  margin: number;
+};
+type TypeStyleMap = { [key in Size]: TypeStyle };
+
+type StatusStyle = {
+  [key in Margin]: TypeStyleMap;
+};
+
+const dividerTypeMap: StatusStyle = {
+  on: {
+    s: { height: 1, margin: 20 },
+    l: { height: 10, margin: 20 },
+  },
+  off: {
+    s: { height: 1, margin: 0 },
+    l: { height: 10, margin: 0 },
+  },
+};
+
+const Divider = ({
+  size = 's',
+  margin = 'off',
+}: {
+  size: Size;
+  margin: Margin;
+}) => {
+  return <StyleDivider size={size} margin={margin} />;
+};
+
+export default Divider;
+
+const StyleDivider = styled.div<{ size: Size; margin: Margin }>`
+  ${({ size, margin }) => `
+        height: ${dividerTypeMap[margin][size].height}px; 
+        margin-left: ${dividerTypeMap[margin][size].margin}px; 
+        margin-right: ${dividerTypeMap[margin][size].margin}px;
+    `}
+  background: ${theme.colors.grey800};
+`;
