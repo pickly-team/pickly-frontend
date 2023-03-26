c30f100bb1fe0c387c5e4734699f000482a65301
ww8007 <wshmin1234@gmail.com>
2023-03-27 01:43:47 +0900

    feat: 카테고리 추가 페이지 비지니스 로직, 페이지 추가

---
+++ src/category/service/hooks/useHandleAddCategory.tsx
@@ -0,0 +1,97 @@
+import useBottomSheet from '@/common-ui/BottomSheet/hooks/useBottomSheet';
+import { ChangeEventHandler, useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+
+export interface CategoryItem {
+  id: string;
+  emoji: string;
+  categoryName: string;
+}
+
+const useHandleAddCategory = () => {
+  // INTERACTION
+  // 1. 사용자 뒤로가기 버튼 클릭
+  const router = useNavigate();
+  const onClickBack = () => {
+    router(-1);
+  };
+
+  // 2. 사용자 저장 버튼 클릭
+  const onClickSave = () => {
+    // TODO : 카테고리 추가 API 호출
+    // TODO : 카테고리 추가 후 메인 페이지로 이동
+    router(-1);
+  };
+
+  // 3. 사용자 카테고리 이모지 변경
+  const [emoji, setEmoji] = useState('😎');
+  const { close, isOpen, open } = useBottomSheet();
+  const onChangeEmoji = useCallback((emoji: string) => {
+    setEmoji(emoji);
+    open();
+  }, []);
+
+  // 4. 사용자 카테고리 이름 변경
+  const [categoryName, setCategoryName] = useState('');
+  const onChangeCategoryName = useCallback<
+    ChangeEventHandler<HTMLInputElement>
+  >((categoryName) => {
+    setCategoryName(categoryName.target.value);
+  }, []);
+
+  // 5. 사용자 카테고리 추가
+  const isAllCategoryInfoFilled = (): boolean =>
+    Boolean(emoji.length && categoryName.length);
+
+  const [categoryList, setCategoryList] = useState<CategoryItem[]>([]);
+  const onClickAddCategory = useCallback(() => {
+    setCategoryList([...categoryList, { emoji, categoryName, id: uuid() }]);
+    setEmoji('😎');
+    setCategoryName('');
+  }, [categoryList, emoji, categoryName]);
+
+  // 6. 사용자 카테고리 삭제
+  const onClickDeleteCategory = useCallback(
+    (id: string) => {
+      console.log('삭제');
+      setCategoryList(categoryList.filter((item) => item.id !== id));
+    },
+    [categoryList],
+  );
+
+  // 7. 사용자 카테고리 수정
+  const onClickEditCategory = useCallback(
+    (id: string) => {
+      console.log('수정');
+      // 1. 수정할 카테고리를 찾는다.
+      const category = categoryList.find((category) => category.id === id);
+      // 2. 수정할 카테고리의 이름과 이모지를 변경한다.
+      if (category) {
+        setEmoji(category.emoji);
+        setCategoryName(category.categoryName);
+      }
+      // 3. 수정할 카테고리를 삭제한다.
+      setCategoryList(categoryList.filter((category) => category.id !== id));
+    },
+    [categoryList, emoji, categoryName],
+  );
+
+  return {
+    emoji,
+    categoryName,
+    categoryList,
+    close,
+    isOpen,
+    isAllCategoryInfoFilled,
+    onClickBack,
+    onClickSave,
+    onChangeEmoji,
+    onChangeCategoryName,
+    onClickAddCategory,
+    onClickDeleteCategory,
+    onClickEditCategory,
+  };
+};
+
+export default useHandleAddCategory;
---
+++ src/pages/CategoryAddPage.tsx
@@ -0,0 +1,66 @@
+import useHandleAddCategory from '@/category/service/hooks/useHandleAddCategory';
+import CategoryAddArea from '@/category/ui/Add/CategoryAddArea';
+import CategoryAddInfo from '@/category/ui/Add/CategoryAddInfo';
+import CategoryInput from '@/category/ui/Add/CategoryInput';
+import Divider from '@/category/ui/Divider';
+import HeaderLeftAndRight from '@/common-ui/Header/HeaderLeftAndRight';
+import getRem from '@/utils/getRem';
+import styled from '@emotion/styled';
+
+// TODO : 이모지 라이브러리 찾아서 연동
+
+const CategoryAddPage = () => {
+  const {
+    categoryList,
+    categoryName,
+    emoji,
+    isAllCategoryInfoFilled,
+    onChangeCategoryName,
+    onClickBack,
+    onClickAddCategory,
+    onClickDeleteCategory,
+    onClickEditCategory,
+    onClickSave,
+  } = useHandleAddCategory();
+  return (
+    <>
+      <HeaderLeftAndRight
+        left={{ type: 'back', onClick: onClickBack }}
+        right={{ text: '저장', onClick: onClickSave }}
+      />
+      {/** 카테고리 페이지 설명 영역 */}
+      <CategoryAddInfo />
+      {/** 카테고리 입력 영역 */}
+      <CategoryInput>
+        <CategoryInput.Emoji emoji={emoji} />
+        <CategoryInput.Name
+          categoryName={categoryName}
+          onChangeCategoryName={onChangeCategoryName}
+        />
+      </CategoryInput>
+      <MarginDivider>
+        <Divider size="s" margin="off" />
+      </MarginDivider>
+      {/** 카테고리 추가 영역 */}
+      <CategoryAddArea categoryList={categoryList}>
+        <CategoryAddArea.BlankCategoryBox
+          onClickAddCategory={onClickAddCategory}
+          isAllCategoryInfoFilled={isAllCategoryInfoFilled}
+        />
+        <CategoryAddArea.CategoryList
+          categoryList={categoryList}
+          onClickDeleteCategory={onClickDeleteCategory}
+          onClickEditCategory={onClickEditCategory}
+          isAllCategoryInfoFilled={isAllCategoryInfoFilled}
+          onClickAddCategory={onClickAddCategory}
+        />
+      </CategoryAddArea>
+    </>
+  );
+};
+
+export default CategoryAddPage;
+
+const MarginDivider = styled.div`
+  margin: ${getRem(40)} 0;
+`;
