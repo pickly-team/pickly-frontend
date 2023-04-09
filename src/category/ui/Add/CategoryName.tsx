9610c6bafbece48a7cddfd3d5897738dd8f25a39
ww8007 <wshmin1234@gmail.com>
2023-04-09 16:24:59 +0900

    fix: CategoryName 컴포넌트 분리

---
+++ src/category/ui/Add/CategoryName.tsx
@@ -0,0 +1,29 @@
+import Input from '@/common-ui/Input';
+import Text from '@/common-ui/Text';
+import getRem from '@/utils/getRem';
+import styled from '@emotion/styled';
+
+interface NameProps {
+  onChangeCategoryName: (name: string) => void;
+  categoryName: string;
+}
+
+const CategoryName = ({ categoryName, onChangeCategoryName }: NameProps) => {
+  return (
+    <>
+      <Text.Header level="h3" weight="bold" fontSize={getRem(16)}>
+        카테고리 이름
+      </Text.Header>
+      <CategoryNameInput
+        value={categoryName}
+        onChange={(e) => onChangeCategoryName(e.target.value)}
+      />
+    </>
+  );
+};
+
+const CategoryNameInput = styled(Input)`
+  margin-top: ${getRem(20)};
+`;
+
+export default CategoryName;
