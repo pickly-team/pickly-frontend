5b312406de4f5f2b77fba104e68fc831f056a2d2
seoyeonkim01 <seoyeon.kim@creatrip.com>
2023-04-23 15:12:17 +0900

    feat: formatDate 유틸 추가, 테스트 코드 작성

---
+++ src/utils/date/formatDate.test.ts
@@ -0,0 +1,60 @@
+import formatDate from '@/utils/date/formatDate';
+
+describe('formatDate test', () => {
+  //test formatDate
+  test('start, end 차이가 1분 미만이면 > 방금전 으로 표시된다.', () => {
+    //given
+    const start = new Date('2020-01-01 00:00:00');
+    const end = new Date('2020-01-01 00:00:30');
+
+    //when
+    const result = formatDate(start, end);
+
+    //then
+    expect(result).toBe('방금전');
+  });
+  test('start, end 차이가 1분 이상 1시간 미만이면 > ${minutes}분전 으로 표시된다.', () => {
+    //given
+    const start = new Date('2020-01-01 00:00:00');
+    const end = new Date('2020-01-01 00:01:00');
+
+    //when
+    const result = formatDate(start, end);
+
+    //then
+    expect(result).toBe('1분전');
+  });
+  test('start, end 차이가 1시간 이상 하루 미만이면 > ${hours}시간전 으로 표시된다.', () => {
+    //given
+    const start = new Date('2020-01-01 00:00:00');
+    const end = new Date('2020-01-01 01:00:00');
+
+    //when
+    const result = formatDate(start, end);
+
+    //then
+    expect(result).toBe('1시간전');
+  });
+  test('start, end 차이가 하루 이상 1년 미만이면 > ${days}일전 으로 표시된다.', () => {
+    //given
+    const start = new Date('2020-01-01 00:00:00');
+    const end = new Date('2020-01-02 00:00:00');
+
+    //when
+    const result = formatDate(start, end);
+
+    //then
+    expect(result).toBe('1일전');
+  });
+  test('start, end 차이가 1년 이상이면 > ${years}년전 으로 표시된다.', () => {
+    //given
+    const start = new Date('2020-01-01 00:00:00');
+    const end = new Date('2021-01-01 00:00:00');
+
+    //when
+    const result = formatDate(start, end);
+
+    //then
+    expect(result).toBe('1년전');
+  });
+});
