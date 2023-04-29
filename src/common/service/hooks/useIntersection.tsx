9843b1dfa085763d20287d78ed351f6a5f4b87e6
ww8007 <wshmin1234@gmail.com>
2023-04-30 02:50:46 +0900

    feat: 무한 스크롤을 위한 useIntersection 커스텀 훅 추가

---
+++ src/common/service/hooks/useIntersection.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+interface Props {
+  target: React.RefObject<HTMLElement>;
+  rootElement?: HTMLElement | null;
+  rootMargin?: string;
+  threshold?: number;
+  onIntersect: (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver,
+  ) => void;
+}
+
+const useIntersection = ({
+  target,
+  rootElement = null,
+  rootMargin = '0px',
+  threshold = 0,
+  onIntersect,
+}: Props) => {
+  useEffect(() => {
+    if (!target.current) return;
+    const io = new IntersectionObserver(onIntersect, {
+      root: rootElement,
+      threshold,
+      rootMargin,
+    });
+    io.observe(target.current);
+
+    return () => io.disconnect();
+  }, [onIntersect, target, threshold]);
+};
+
+export default useIntersection;
