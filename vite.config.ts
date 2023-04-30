b1e8cdf107fb8115a231a89ab8cdc4f8f17f6ca1
ww8007 <wshmin1234@gmail.com>
2023-05-01 02:10:06 +0900

    feat: env 사용을 위한 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,50 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-  ],
-  resolve: {
-    //TODO: alias 패턴 변경
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    esbuild: {
+      target: 'ESNext',
     },
-  },
-  server: {
-    proxy: {
-      '/v1': {
-        target: 'http://localhost:8080/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v1/, ''),
-        secure: false,
-        ws: true,
+    build: {
+      target: 'ESNext',
+    },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+    ],
+    resolve: {
+      //TODO: alias 패턴 변경
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/v1': {
+          target:
+            mode === 'production'
+              ? env.VITE_SERVER_URI
+              : 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/v1/, ''),
+          secure: false,
+          ws: true,
+        },
+      },
+    },
+    define: {
+      'process.env': env,
+      'process.env.NODE_ENV': JSON.stringify(mode),
+      'process.env.VITE_SERVER_URI': JSON.stringify(env.VITE_SERVER_URI),
+    },
+  };
 });
