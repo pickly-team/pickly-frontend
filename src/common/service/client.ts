24eecf1cf1ae84742c6e8f05002ce9c4cf5a2017
ww8007 <wshmin1234@gmail.com>
2023-05-15 05:50:45 +0900

    fix: dev, production 환경에 따른 baseURL 분리

--- src/common/service/client.ts
+++ src/common/service/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: process.env.VITE_SERVER_URI,
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VITE_SERVER_URI
+      : 'http://localhost:8080/api',
 });
 
 export default client;
