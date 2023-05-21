495da30f4ee73b54bd363ebf602ec71a9f028e24
ww8007 <wshmin1234@gmail.com>
2023-05-22 00:51:20 +0900

    feat: 단일 카테고리 조회 API 추가

--- src/category/api/category.ts
+++ src/category/api/category.ts
@@ -1,6 +1,6 @@
 import client from '@/common/service/client';
 import { navigatePath } from '@/constants/navigatePath';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 const DOMAIN = 'CATEGORY';
 
 export const GET_CATEGORY_LIST = (memberId: number) => [
@@ -9,6 +9,49 @@ export const GET_CATEGORY_LIST = (memberId: number) => [
   'CATEGORY_LIST',
   memberId,
 ];
+
+export const GET_CATEGORY = (memberId: number, categoryId: string) => [
+  navigatePath.CATEGORY_EDIT,
+  DOMAIN,
+  'CATEGORY',
+  memberId,
+  categoryId,
+];
+
+// 단일 카테고리 조회
+interface CategoryItem {
+  categoryId: number;
+  name: string;
+  emoji: string;
+  orderNum: number;
+}
+
+const getCategoryAPI = async (categoryId: string) => {
+  const { data } = await client.get<CategoryItem>(`/categories/${categoryId}`);
+  return data;
+};
+
+interface GETCategoryRequest {
+  categoryId: string;
+  memberId: number;
+  mode: 'ADD' | 'EDIT';
+}
+
+export const useGETCategoryAPI = ({
+  categoryId,
+  memberId,
+  mode,
+}: GETCategoryRequest) => {
+  return useQuery(
+    GET_CATEGORY(memberId, categoryId),
+    () => getCategoryAPI(categoryId),
+    {
+      staleTime: 1000,
+      cacheTime: 1000 * 60 * 5,
+      enabled: mode === 'EDIT',
+    },
+  );
+};
 
 // 카테고리 리스트 조회
 
