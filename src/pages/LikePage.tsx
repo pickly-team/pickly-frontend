7c9dacae983190c7fe09e26eed2591ed29632a88
ww8007 <wshmin1234@gmail.com>
2023-05-29 00:48:43 +0900

    refactor: Suspense를 이용한 방식으로 리펙터링

--- src/pages/LikePage.tsx
+++ src/pages/LikePage.tsx
@@ -2,31 +2,21 @@ import styled from '@emotion/styled';
 
 import getRem from '@/utils/getRem';
 import Header from '@/common-ui/Header/Header';
-import Icon from '@/common-ui/assets/Icon';
-import Text from '@/common-ui/Text';
-import useBookMarkLikeItemHandler from '@/bookmarks/service/hooks/useBookMarkLikeItemHandler';
-import BookmarkLikeItem from '@/bookmarks/ui/BookmarkLikeItem';
+import { Suspense } from 'react';
+import BookmarkLikeList from '@/bookmarks/ui/Like/BookmarkLikeList';
+import SkeletonBookmarkLikeList from '@/bookmarks/ui/Like/SkeletonBookmarkLikeList';
 
 const LikePage = () => {
   const showBackButton = true;
   const title = '좋아요 목록';
-  const rightButton = <Icon name="more" size="s" />;
-
-  const { bookMarkList, isLoading } = useBookMarkLikeItemHandler();
 
   return (
     <Layout>
-      <Header
-        title={title}
-        showBackButton={showBackButton}
-        rightButton={rightButton}
-      />
-
+      <Header title={title} showBackButton={showBackButton} />
       <LBody>
-        {isLoading && <Text.Span>로딩중...</Text.Span>}
-        {bookMarkList.map((bookmark) => (
-          <BookmarkLikeItem key={bookmark.id} {...bookmark} />
-        ))}
+        <Suspense fallback={<SkeletonBookmarkLikeList />}>
+          <BookmarkLikeList />
+        </Suspense>
       </LBody>
     </Layout>
   );
@@ -36,7 +26,6 @@ export default LikePage;
 
 const Layout = styled.div``;
 const LBody = styled.div`
-  padding: ${getRem(0, 20)};
   margin-top: ${getRem(12)};
   row-gap: ${getRem(21)};
 `;
