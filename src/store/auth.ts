93e05843e4ae6d2914689711240b0e735d3b50ca
ww8007 <wshmin1234@gmail.com>
2023-07-11 06:44:34 +0900

    feat: api 연동에 따른 user 정보 추가

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,16 +1,51 @@
 import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+export interface UserInfo {
+  id: number;
+  name: string;
+  nickname: string;
+  profileEmoji: string;
+  followersCount: number;
+  followeesCount: number;
+  bookmarksCount: number;
+}
 
 interface Auth {
   isLogin: boolean;
-  userToken: string;
-  login: (userToken: string) => void;
+  token: string;
+  memberId: number;
+  userInfo: UserInfo;
+  setUserInfo: (userInfo: UserInfo) => void;
+  login: (token: string, memberId: number) => void;
 }
 
-const useAuthStore = create<Auth>((set) => ({
-  isLogin: false,
-  userToken: '',
-  login: (userToken: string) =>
-    set(() => ({ isLogin: true, userToken: userToken })),
-}));
+const useAuthStore = create<Auth>()(
+  devtools(
+    persist(
+      (set) => ({
+        isLogin: false,
+        token: '',
+        memberId: 0,
+        userInfo: {
+          id: 0,
+          name: '',
+          nickname: '',
+          profileEmoji: '😃',
+          followersCount: 0,
+          followeesCount: 0,
+          bookmarksCount: 0,
+        },
+        setUserInfo: (userInfo) => {
+          set({ userInfo });
+        },
+        login: (token, memberId) => {
+          set({ isLogin: true, token, memberId });
+        },
+      }),
+      { name: 'auth' },
+    ),
+  ),
+);
 
 export default useAuthStore;
