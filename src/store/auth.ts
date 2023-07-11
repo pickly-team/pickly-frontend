79e236165809fdf36d9551d15a8e56f7d57573de
ww8007 <wshmin1234@gmail.com>
2023-07-12 06:24:52 +0900

    fix: persist 제거 및 login 인자 객체형식으로 변경

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 
 export interface UserInfo {
   id: number;
@@ -17,34 +17,32 @@ interface Auth {
   memberId: number;
   userInfo: UserInfo;
   setUserInfo: (userInfo: UserInfo) => void;
-  login: (token: string, memberId: number) => void;
+  login: ({ token, memberId }: { token: string; memberId: number }) => void;
 }
 
 const useAuthStore = create<Auth>()(
   devtools(
-    persist(
-      (set) => ({
-        isLogin: false,
-        token: '',
-        memberId: 0,
-        userInfo: {
-          id: 0,
-          name: '',
-          nickname: '',
-          profileEmoji: '😃',
-          followersCount: 0,
-          followeesCount: 0,
-          bookmarksCount: 0,
-        },
-        setUserInfo: (userInfo) => {
-          set({ userInfo });
-        },
-        login: (token, memberId) => {
-          set({ isLogin: true, token, memberId });
-        },
-      }),
-      { name: 'auth' },
-    ),
+    (set) => ({
+      isLogin: false,
+      token: '',
+      memberId: 1,
+      userInfo: {
+        id: 1,
+        name: '',
+        nickname: '',
+        profileEmoji: '😃',
+        followersCount: 0,
+        followeesCount: 0,
+        bookmarksCount: 0,
+      },
+      setUserInfo: (userInfo) => {
+        set({ userInfo });
+      },
+      login: ({ token, memberId }) => {
+        set({ isLogin: true, token, memberId });
+      },
+    }),
+    { name: 'auth' },
   ),
 );
 
