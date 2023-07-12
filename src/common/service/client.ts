3acccfcb586eef783e5085f4cad68c9b39a48306
ww8007 <wshmin1234@gmail.com>
2023-07-13 03:41:55 +0900

    feat: errorBoundary 처리를 위한 interceptors 추가

--- src/common/service/client.ts
+++ src/common/service/client.ts
@@ -1,8 +1,34 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const client = axios.create({
   baseURL: process.env.VITE_SERVER_URI,
 });
+
+client.interceptors.response.use(
+  (response: AxiosResponse): AxiosResponse => {
+    // 응답을 그대로 리턴
+    return response;
+  },
+  (error: AxiosError): Promise<never> => {
+    // Axios 에러 처리
+    if (error.response) {
+      // 서버가 에러 상태 코드를 리턴한 경우
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // 요청이 만들어졌지만 응답을 받지 못한 경우
+      console.log(error.request);
+    } else {
+      // 요청을 만드는 중에 다른 오류가 발생한 경우
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+
+    // 처리한 후 에러를 다시 throw
+    return Promise.reject(error);
+  },
+);
 
 export default client;
 
