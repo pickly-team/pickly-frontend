c49031f2d994c0f3dc9bb8962e79d4f7394aa079
ww8007 <wshmin1234@gmail.com>
2023-07-14 13:13:07 +0900

    feat: 북마크 정보 API 연동 1. 북마크 정보 조회 2. 북마크 좋아요 3. 북마크 좋아요 해제

--- src/bookmarks/ui/BookmarkArticle.tsx
+++ src/bookmarks/ui/BookmarkArticle.tsx
@@ -3,56 +3,93 @@ import Button from '@/common-ui/Button';
 import Icon from '@/common-ui/assets/Icon';
 import Text from '@/common-ui/Text';
 import styled from '@emotion/styled';
-import { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { ReactNode, SyntheticEvent } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { theme } from '@/styles/theme';
+import BookmarkLikeButton from './Like/BookmarkLikeButton';
+import {
+  useDELETEBookmarkLikeQuery,
+  useGETBookmarkDetailQuery,
+  usePOSTBookmarkLikeQuery,
+} from '../api/bookmark';
+import { timeStampToDate } from '@/utils/date/timeConverter';
+import CommentCountInfo from '@/comment/ui/bookmark/CommentCountInfo';
+import useCommentStore from '@/store/comment';
 
-interface BookMarkArticleProps {
-  previewImgSrc: string;
-  title: string;
-  category: string;
-  createdAt: string;
-  bookMarkUrl: string;
-  likeButton: ReactNode;
-  messageInfo: ReactNode;
-}
+const BookMarkArticle = () => {
+  const { id: bookmarkId } = useParams<{ id: string }>();
+  const { data: bookmarkDetail } = useGETBookmarkDetailQuery({
+    bookmarkId: bookmarkId ?? '',
+  });
+  const { commentCount } = useCommentStore();
 
-const BookMarkArticle = ({
-  previewImgSrc,
-  title,
-  category,
-  createdAt,
-  bookMarkUrl,
-  likeButton,
-  messageInfo,
-}: BookMarkArticleProps) => {
+  const onErrorImage = (e: SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = '/images/main.png';
+    e.currentTarget.setAttribute('style', 'object-fit: contain');
+  };
+
+  const { mutate: postBookmarkLike } = usePOSTBookmarkLikeQuery({
+    bookmarkId: bookmarkId ?? '',
+  });
+  const { mutate: postBookmarkDislike } = useDELETEBookmarkLikeQuery({
+    bookmarkId: bookmarkId ?? '',
+  });
+
+  const onClickLike = () => {
+    postBookmarkLike({ bookmarkId: bookmarkId ?? '' });
+  };
+
+  const onClickDislike = () => {
+    postBookmarkDislike({ bookmarkId: bookmarkId ?? '' });
+  };
+
   return (
     <Container>
-      <BookMarkImage src={previewImgSrc} />
-      <BookMarkTitle level="h1" fontSize={2} weight="bold">
-        {title}
+      <BookMarkImage
+        src={bookmarkDetail?.previewImageUrl ?? ''}
+        onError={onErrorImage}
+      />
+      <BookMarkTitle level="h1" fontSize={1.5} weight="bold">
+        {bookmarkDetail?.title ?? ''}
       </BookMarkTitle>
       <CategoryAndIconsWrapper>
         <CategoryButtonWrapper>
-          <Button>{category}</Button>
+          <CategoryButton height={2.5} buttonColor="lightPrimary">
+            <Text.Span color="black" weight="bold">
+              {bookmarkDetail?.categoryName ?? ''}
+            </Text.Span>
+          </CategoryButton>
         </CategoryButtonWrapper>
         <LikeAndMessageIconWrapper>
-          {likeButton}
-          {messageInfo}
+          <BookmarkLikeButton
+            isLike={bookmarkDetail?.isUserLike ?? false}
+            onClickLike={onClickLike}
+            onClickDislike={onClickDislike}
+          />
+          <CommentCountInfo commentCount={commentCount ?? 0} />
         </LikeAndMessageIconWrapper>
       </CategoryAndIconsWrapper>
       <BookMarkInfoWrapper>
         <BookMarkInfo
           description="등록일자"
-          icon={<Icon name="calendar-plus" size="m" />}
-          content={<Text.Span>{createdAt}</Text.Span>}
+          icon={<Icon name="calendar-plus" size="s" />}
+          content={
+            <Text.Span fontSize={0.75}>
+              {timeStampToDate(bookmarkDetail?.createdAt ?? 0)}
+            </Text.Span>
+          }
         />
         <BookMarkInfo
           description="원본 URL"
-          icon={<Icon name="location" size="m" />}
+          icon={<Icon name="location" size="s" />}
           content={
-            <Link to={bookMarkUrl} target={'_blank'} rel={'noreferrer'}>
-              <Text.Span>{bookMarkUrl}</Text.Span>
-            </Link>
+            <BookmarkUrl
+              to={bookmarkDetail?.url ?? ''}
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              <Text.Span fontSize={0.75}>{bookmarkDetail?.url ?? ''}</Text.Span>
+            </BookmarkUrl>
           }
         />
       </BookMarkInfoWrapper>
@@ -73,9 +110,11 @@ const BookMarkInfo = ({
 }) => {
   return (
     <InfoRow>
-      {icon}
+      <IconWrapper>{icon}</IconWrapper>
       <InfoTextWrapper>
-        <InfoDescriptionText>{description}</InfoDescriptionText>
+        <InfoDescriptionText fontSize={0.875}>
+          {description}
+        </InfoDescriptionText>
         {content}
       </InfoTextWrapper>
     </InfoRow>
@@ -104,6 +143,14 @@ const CategoryButtonWrapper = styled.div`
   width: ${getRem(154)};
 `;
 
+const CategoryButton = styled(Button)`
+  color: ${theme.colors.black};
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+`;
+
 const LikeAndMessageIconWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -111,23 +158,30 @@ const LikeAndMessageIconWrapper = styled.div`
 `;
 
 const BookMarkInfoWrapper = styled.div`
-  padding: ${getRem(10)};
-  > * {
-    margin-bottom: ${getRem(10)};
-  }
+  padding: ${getRem(20)} 0px;
 `;
 const InfoRow = styled.div`
   display: flex;
   align-items: center;
   column-gap: ${getRem(10)};
+  width: 100%;
+  overflow: hidden;
+`;
+
+const BookmarkUrl = styled(Link)`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  margin-top: -0.3rem;
+  max-width: 70%;
 `;
 
 const InfoDescriptionText = styled(Text.Span)`
-  display: block;
   width: ${getRem(70)};
 `;
 const InfoTextWrapper = styled.div`
   width: 100%;
   display: flex;
-  column-gap: ${getRem(35)};
+  height: 2rem;
+  align-items: center;
 `;
