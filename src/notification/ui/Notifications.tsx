40f7cbd2a14504355acde3d310dea998cdd70663
ww8007 <wshmin1234@gmail.com>
2023-07-15 23:21:41 +0900

    feat: 알림 페이지 API 연동 1. 알림 내역 조회 2. 알림 읽음 처리 3. 알림 삭제

--- src/notification/ui/Notifications.tsx
+++ src/notification/ui/Notifications.tsx
@@ -1,20 +1,52 @@
 import NotificationSlideItem from '@/notification/ui/skeleton/NotificationSlideItem';
-import { useGetNotifications } from '@/notification/api/notification';
+import {
+  useDELETENotificationQuery,
+  useGETNotificationListQuery,
+  usePATCHNotificationReadQuery,
+} from '@/notification/api/notification';
+import useAuthStore from '@/store/auth';
 
 const Notifications = () => {
-  const { data } = useGetNotifications();
+  // FIRST RENDER
+  const { memberId } = useAuthStore();
+  const { data: notificationList } = useGETNotificationListQuery({ memberId });
+
+  // USER INTERACTION
+  // 1. 알림 읽음 처리
+  const { mutate: updateReadNotification } = usePATCHNotificationReadQuery({
+    memberId,
+  });
+  const onClickReadNotification = (
+    notificationId: number,
+    isChecked: boolean,
+  ) => {
+    !isChecked && updateReadNotification({ notificationId });
+  };
+
+  // 2. 알림 삭제
+  const { mutate: deleteNotification } = useDELETENotificationQuery({
+    memberId,
+  });
+  const onClickDeleteNotification = (notificationId: number) => {
+    deleteNotification({ notificationId });
+  };
 
   return (
     <>
-      {data?.map((notification) => (
+      {notificationList?.map((notification) => (
         <NotificationSlideItem
           key={notification.id}
           bookMarkInfo={{
-            id: notification.id,
-            title: notification.title,
+            id: String(notification.bookmarkId),
+            content: notification.content,
           }}
           createdAt={notification.createdAt}
-          isRead={notification.isRead}
+          isRead={notification.isChecked}
+          title={notification.title}
+          toggleReadNotification={() =>
+            onClickReadNotification(notification.id, notification.isChecked)
+          }
+          deleteNotification={() => onClickDeleteNotification(notification.id)}
         />
       ))}
     </>
