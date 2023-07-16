928ad76dfc9495105de94637c7d59c17ce59a157
ww8007 <wshmin1234@gmail.com>
2023-07-17 03:58:09 +0900

    feat: 친구 찾기 페이지 이동 아이콘 추가

--- src/pages/FriendPage.tsx
+++ src/pages/FriendPage.tsx
@@ -2,11 +2,24 @@ import Header from '@/common-ui/Header/Header';
 import Friends from '@/friend/ui/Friends';
 import { Suspense } from 'react';
 import FriendSkeletonItem from '@/friend/ui/FriendSkeletonItem';
+import IconButton from '@/common/ui/IconButton';
+import { useNavigate } from 'react-router-dom';
+import { navigatePath } from '@/constants/navigatePath';
 
 const FriendPage = () => {
+  const router = useNavigate();
   return (
     <>
-      <Header title={'친구 목록'} />
+      <Header
+        title={'친구 목록'}
+        rightButton={
+          <IconButton
+            name="search"
+            size="m"
+            onClick={() => router(navigatePath.FRIEND_SEARCH)}
+          />
+        }
+      />
       <Suspense
         fallback={[1, 2, 3].map((item) => (
           <FriendSkeletonItem key={item} />
