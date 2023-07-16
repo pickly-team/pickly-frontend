577f062f77c325fad2caeaf67641014d462bebf2
ww8007 <wshmin1234@gmail.com>
2023-07-17 05:41:10 +0900

    fix: api friends로 이전

--- src/common/service/client.ts
+++ src/common/service/client.ts
@@ -31,18 +31,3 @@ client.interceptors.response.use(
 );
 
 export default client;
-
-export const api = {
-  deleteUnfollowFriend: (memberId: string, followingId: string) => {
-    return client.delete(`/members/${memberId}/following/${followingId}`);
-  },
-  postFollowFriend: (memberId: string, followingId: string) => {
-    return client.post(`/members/${memberId}/following/${followingId}`);
-  },
-  getFollowings: (memberId: string, cursorId?: string, pageSize?: number) => {
-    return client.get(`/members/${memberId}/followings`);
-  },
-  getFollowers: (memberId: string, cursorId?: string, pageSize?: number) => {
-    return client.get(`/members/${memberId}/followers`);
-  },
-};
