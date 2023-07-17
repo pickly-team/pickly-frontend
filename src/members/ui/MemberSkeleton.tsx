09e139d2e3b7b1816cefb4ce647fc8e86ff7c86f
ww8007 <wshmin1234@gmail.com>
2023-07-17 14:55:42 +0900

    feat: Member Skeleton ui 컴포넌트 추가

---
+++ src/members/ui/MemberSkeleton.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import getRem from '@/utils/getRem';
+import {
+  skeletonBackgroundStyle,
+  skeletonContentStyle,
+} from '@/common-ui/utils/skeletonStyles';
+
+const MemberSkeleton = () => {
+  return (
+    <Container>
+      <TitleItem />
+    </Container>
+  );
+};
+
+export default MemberSkeleton;
+
+const Container = styled.div`
+  height: ${getRem(45)};
+  padding: ${getRem(12, 20)};
+  width: 100%;
+  ${skeletonBackgroundStyle};
+  margin-bottom: ${getRem(10)};
+`;
+
+const TitleItem = styled.div`
+  height: ${getRem(16)};
+  ${skeletonContentStyle};
+`;
