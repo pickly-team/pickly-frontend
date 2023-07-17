4c9670d300f1089e60f0862ae91d7750726b5a14
ww8007 <wshmin1234@gmail.com>
2023-07-17 14:57:35 +0900

    feat: 차단 유저 목록 UI 구현 및 API 연동 1. 차단한 멤버 리스트 2. 차단 해제

---
+++ src/members/ui/BlockMemberList.tsx
@@ -0,0 +1,77 @@
+import { useGETBlockMemberListQuery, useUnblockUserQuery } from '../api/member';
+import useAuthStore from '@/store/auth';
+import Member from './Member';
+import styled from '@emotion/styled';
+import Button from '@/common-ui/Button';
+import getRem from '@/utils/getRem';
+import { theme } from '@/styles/theme';
+import useBottomIntersection from '@/common/service/hooks/useBottomIntersection';
+import MemberSkeleton from './MemberSkeleton';
+
+const BlockMemberList = () => {
+  // FIRST RENDER
+  const { memberId } = useAuthStore();
+
+  // SERVER
+  // 1. 차단한 멤버 리스트
+  const {
+    data: blockMemberList,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useGETBlockMemberListQuery({
+    memberId,
+  });
+  const flatBlockMemberList =
+    blockMemberList?.pages.flatMap((page) => page.contents) ?? [];
+
+  const { bottom } = useBottomIntersection({
+    fetchNextPage,
+    enabled: !isFetchingNextPage,
+  });
+
+  // 2. 차단 해제
+  const { mutate: unblockUser } = useUnblockUserQuery({ memberId });
+  const onClick = (blockeeId: number) => {
+    unblockUser({ blockerId: memberId, blockeeId });
+  };
+
+  return (
+    <Wrapper>
+      {flatBlockMemberList.map((info) => (
+        <Member
+          key={info.id}
+          emoji={info.profileEmoji}
+          name={info.name}
+          button={
+            <StyledButton
+              onClick={() => onClick(info.id)}
+              buttonColor="lightPrimary"
+            >
+              차단 해제
+            </StyledButton>
+          }
+        />
+      ))}
+      <div ref={bottom} />
+      {isFetchingNextPage &&
+        Array.from({ length: 10 }).map((_, idx) => (
+          <MemberSkeleton key={idx} />
+        ))}
+    </Wrapper>
+  );
+};
+
+export default BlockMemberList;
+
+const Wrapper = styled.div`
+  padding: 0 ${getRem(20)};
+`;
+
+const StyledButton = styled(Button)`
+  width: 5rem;
+  font-size: ${getRem(14)};
+  padding: 0.25rem 0.94rem;
+  height: fit-content;
+  color: ${theme.colors.black};
+  font-weight: bold;
+`;
