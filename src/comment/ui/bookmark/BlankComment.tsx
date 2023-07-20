0ba0a4fdc70c38ea898765cf249f31c96157ee77
ww8007 <wshmin1234@gmail.com>
2023-07-20 20:24:22 +0900

    feat: 댓글이 없는 경우 보여줄 UI 컴포넌트 추가

---
+++ src/comment/ui/bookmark/BlankComment.tsx
@@ -0,0 +1,49 @@
+import Text from '@/common-ui/Text';
+import Icon from '@/common-ui/assets/Icon';
+import { theme } from '@/styles/theme';
+import getRem from '@/utils/getRem';
+import styled from '@emotion/styled';
+
+const BlankComment = () => {
+  return (
+    <Container>
+      <IconWrapper>
+        <Icon name="circle-pencil" size="l" />
+      </IconWrapper>
+      <TextWrapper>
+        <BlankText fontSize={0.8} weight="bold">
+          댓글이 없습니다.
+        </BlankText>
+        <BlankText fontSize={0.8} weight="bold">
+          첫 댓글을 남겨보세요!
+        </BlankText>
+      </TextWrapper>
+    </Container>
+  );
+};
+
+export default BlankComment;
+
+const Container = styled.div`
+  display: grid;
+  flex-direction: column;
+  row-gap: 0.6rem;
+  padding: ${getRem(15, 20)};
+  border-radius: ${getRem(7)};
+  background-color: ${theme.colors.grey800};
+  margin-bottom: 1rem;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.2rem;
+  align-items: center;
+`;
+
+const BlankText = styled(Text.Span)``;
