20ef2ac60342922b179db170354ff1ded5d3c2ee
ww8007 <wshmin1234@gmail.com>
2023-07-25 03:36:45 +0900

    fix: https에 대한 내용만 잘라내도록 정규식 수정

--- src/utils/checkValidateURL.ts
+++ src/utils/checkValidateURL.ts
@@ -1,8 +1,8 @@
 const checkValidateURL = (url: string) => {
-  const urlRegex =
-    '^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$';
-  const regex = new RegExp(urlRegex);
-  return regex.test(url);
+  const regex = /https?:\/\/[^\s]+/g;
+
+  const result = url.match(regex);
+  return result ? result[0] : '';
 };
 
 export default checkValidateURL;
