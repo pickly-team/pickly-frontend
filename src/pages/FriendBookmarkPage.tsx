10da57eac99fbc074f3a1daac2d077be46569ba5
ww8007 <wshmin1234@gmail.com>
2023-07-26 03:16:12 +0900

    fix: 유저 차단 여부에 따른 차단하기, 차단해제 워딩 및 메서드 수정

--- src/pages/FriendBookmarkPage.tsx
+++ src/pages/FriendBookmarkPage.tsx
@@ -13,17 +13,25 @@ import IconButton from '@/common/ui/IconButton';
 import {
   useGETFriendProfileQuery,
   usePOSTBlockMemberQuery,
+  useUnblockUserQuery,
 } from '@/members/api/member';
 import useAuthStore from '@/store/auth';
 import BookmarkListView from '@/bookmarks/ui/Main/BookmarkListView';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import SkeletonWrapper from '@/common-ui/SkeletonWrapper';
 import BookmarkSkeletonItem from '@/bookmarks/ui/Main/BookmarkSkeletonItem';
+import useFriendStore from '@/store/friend';
 
 const FriendBookmarkPage = () => {
   // FIRST RENDER
   const { memberId } = useAuthStore();
   const { id: friendId } = useParams<{ id: string }>();
+
+  const { setFriendId } = useFriendStore();
+
+  useEffect(() => {
+    setFriendId(Number(friendId));
+  }, [friendId]);
 
   // SERVER
   // 1. 친구 프로필 조회
@@ -37,6 +45,10 @@ const FriendBookmarkPage = () => {
   const { mutate: postBlockMember } = usePOSTBlockMemberQuery({ memberId });
   const onClick_차단하기 = () => {
     postBlockMember({ blockeeId: Number(friendId), blockerId: memberId });
+  };
+  const { mutate: deleteUnBlockMember } = useUnblockUserQuery({ memberId });
+  const onClick_차단해제 = () => {
+    deleteUnBlockMember({ blockeeId: Number(friendId), blockerId: memberId });
   };
 
   // 2. 카테고리 선택
@@ -59,21 +71,29 @@ const FriendBookmarkPage = () => {
               as={<IconButton onClick={() => {}} name="more" size="s" />}
             />
             <TriggerBottomSheet.BottomSheet>
-              <TriggerBottomSheet.Item onClick={onClick_차단하기}>
-                차단하기
-              </TriggerBottomSheet.Item>
+              {!!profileInfo?.isBlocked && (
+                <TriggerBottomSheet.Item onClick={onClick_차단해제}>
+                  차단해제
+                </TriggerBottomSheet.Item>
+              )}
+              {!profileInfo?.isBlocked && (
+                <TriggerBottomSheet.Item onClick={onClick_차단하기}>
+                  차단하기
+                </TriggerBottomSheet.Item>
+              )}
             </TriggerBottomSheet.BottomSheet>
           </TriggerBottomSheet>
         }
       />
       <LTop>
         <BookmarkUserInfo
-          userEmoji={profileInfo?.profileEmoji || ''}
-          userName={profileInfo?.nickname || ''}
+          userEmoji={profileInfo?.profileEmoji ?? ''}
+          userName={profileInfo?.nickname ?? ''}
           isFriendPage={{
-            isFollowing: profileInfo?.isFollowing || false,
+            isFollowing: profileInfo?.isFollowing ?? false,
             friendId: Number(friendId),
             memberId,
+            isBlocked: profileInfo?.isBlocked ?? false,
           }}
         />
       </LTop>
