3163e76fb9b4fe05c2a182c6b95912df5ce2e7bf
ww8007 <wshmin1234@gmail.com>
2023-07-27 03:25:20 +0900

    fix: 읽음 상태 이모지 수정

--- src/bookmarks/service/hooks/home/useReadList.tsx
+++ src/bookmarks/service/hooks/home/useReadList.tsx
@@ -1,18 +1,18 @@
 import { ReadType } from '@/bookmarks/ui/Main/BookmarkToggle';
 import useBookmarkStore from '@/store/bookmark';
 
-export type READ_OPTION = '📖 전체' | '📖 읽음' | '📖 읽지 않음';
+export type READ_OPTION = '📖 전체' | '👀 읽음' | '🫣 읽지 않음';
 
 const readSelectOptions: Record<READ_OPTION, string> = {
   '📖 전체': '📖 전체',
-  '📖 읽음': '📖 읽음',
-  '📖 읽지 않음': '📖 읽지 않음',
+  '👀 읽음': '👀 읽음',
+  '🫣 읽지 않음': '🫣 읽지 않음',
 } as const;
 
 export const READ_OPTIONS: Record<READ_OPTION, boolean | null> = {
   '📖 전체': null,
-  '📖 읽음': true,
-  '📖 읽지 않음': false,
+  '👀 읽음': true,
+  '🫣 읽지 않음': false,
 } as const;
 
 const useReadList = () => {
