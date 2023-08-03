27f6114cdfe36fad9019108efddaaf65d969c1c6
ww8007 <wshmin1234@gmail.com>
2023-08-04 01:38:15 +0900

    refactor: 페이지 전환 에니메이션을 위해 stackflow 사용에 따른 변경사항 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import { theme } from '@/styles/theme';
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import IconLoader from './common-ui/assets/IconLoader';
-import MainPage from './pages/MainPage';
-import LikePage from './pages/LikePage';
 import GlobalStyle from './styles/GlobalStyle';
-import { navigatePath } from '@/constants/navigatePath';
-import BookMarkDetailPage from '@/pages/BookMarkDetailPage';
-import Layout from '@/common-ui/Layout';
-import FaqPage from '@/pages/FaqPage';
-import UserCreatePage from './pages/UserInfoPage';
-import CategoryAddPage from './pages/CategoryManagePage';
-import ReportPage from './pages/ReportPage';
-import CommentPage from './pages/CommentPage';
-import ProfilePage from '@/pages/ProfilePage';
-import CategoryListPage from './pages/CategoryListPage';
 import ToastList from './common-ui/Toast/ToastList';
-import NotificationPage from '@/pages/NotificationPage';
 import { Analytics } from '@vercel/analytics/react';
-import FriendPage from '@/pages/FriendPage';
-import FriendBookmarkPage from './pages/FriendBookmarkPage';
 import ApiErrorBoundary from './common-ui/Error/ApiErrorBoundary';
-import FriendSearchPage from './pages/FriendSearchPage';
 import RNListener from './RNListener';
-import BlockUserListPage from './pages/BlockUserListPage';
-import IntroducePage from './pages/IntroducePage';
+import { Stack } from './common-ui/stackflow';
+import '@stackflow/plugin-basic-ui/index.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -45,73 +28,12 @@ const App = () => {
         <GlobalStyle />
         <IconLoader />
         <ToastList />
-        <BrowserRouter>
-          <RNListener />
-          <ApiErrorBoundary>
-            <Layout>
-              <Routes>
-                <Route
-                  path={navigatePath.INTRODUCE}
-                  element={<IntroducePage />}
-                />
-                <Route path={navigatePath.MAIN} element={<MainPage />} />
-                <Route
-                  path={navigatePath.BOOKMARK_DETAIL}
-                  element={<BookMarkDetailPage />}
-                />
-                <Route path={navigatePath.FAQ} element={<FaqPage />} />
-                <Route
-                  path={navigatePath.COMMENT_REPORT}
-                  element={<ReportPage mode="COMMENT" />}
-                />
-                <Route
-                  path={navigatePath.BOOKMARK_REPORT}
-                  element={<ReportPage mode="BOOKMARK" />}
-                />
-                <Route
-                  path={navigatePath.USER}
-                  element={<UserCreatePage mode="CREATE" />}
-                />
-                <Route
-                  path={navigatePath.NOTIFICATION}
-                  element={<NotificationPage />}
-                />
-                <Route
-                  path={navigatePath.USER_EDIT}
-                  element={<UserCreatePage mode="EDIT" />}
-                />
-                <Route
-                  path={navigatePath.CATEGORY_LIST}
-                  element={<CategoryListPage />}
-                />
-                <Route
-                  path={navigatePath.CATEGORY_ADD}
-                  element={<CategoryAddPage mode="ADD" />}
-                />
-                <Route
-                  path={navigatePath.CATEGORY_EDIT}
-                  element={<CategoryAddPage mode="EDIT" />}
-                />
-                <Route path={navigatePath.COMMENT} element={<CommentPage />} />
-                <Route path={navigatePath.PROFILE} element={<ProfilePage />} />
-                <Route path={navigatePath.LIKE_PAGE} element={<LikePage />} />
-                <Route path={navigatePath.FRIEND} element={<FriendPage />} />
-                <Route
-                  path={navigatePath.FRIEND_SEARCH}
-                  element={<FriendSearchPage />}
-                />
-                <Route
-                  path={navigatePath.FRIEND_BOOKMARK}
-                  element={<FriendBookmarkPage />}
-                />
-                <Route
-                  path={navigatePath.BLOCK_USER}
-                  element={<BlockUserListPage />}
-                />
-              </Routes>
-            </Layout>
-          </ApiErrorBoundary>
-        </BrowserRouter>
+
+        <RNListener />
+        <ApiErrorBoundary>
+          <Stack />
+        </ApiErrorBoundary>
+
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
       <Analytics />
