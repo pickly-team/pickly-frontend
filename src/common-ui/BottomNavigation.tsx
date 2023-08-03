83bf5254a7b852725ed9ff42373c68fb4d5d8234
ww8007 <wshmin1234@gmail.com>
2023-08-04 01:46:36 +0900

    fix: 북마크 추가 페이지로 이동에 따른 변경사항 반영

--- src/common-ui/BottomNavigation.tsx
+++ src/common-ui/BottomNavigation.tsx
@@ -4,20 +4,8 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Icon from './assets/Icon';
 import { BOTTOM_NAVIGATION_Z_INDEX } from '@/constants/zIndex';
-import BookmarkAddBS from '@/bookmarks/ui/Main/BookmarkAddBS';
-import useBottomSheet from './BottomSheet/hooks/useBottomSheet';
-import useInputUrl from '@/bookmarks/service/hooks/add/useInputUrl';
-import useSelectCategory from '@/bookmarks/service/hooks/add/useSelectCategory';
-import useSelectPublishScoped from '@/bookmarks/service/hooks/add/useSelectPublishScoped';
-import useCategoryList from '@/bookmarks/service/hooks/add/useCategoryList';
-import { usePOSTBookmarkMutation } from '@/bookmarks/api/bookmark';
-import checkValidateURL from '@/utils/checkValidateURL';
-import ToastList from './Toast/ToastList';
-import useAuthStore from '@/store/auth';
 import { useActivity } from '@stackflow/react';
 import { useFlow } from './stackflow';
-
-// TODO : 네비게이터에 대한 path를 재정의 필요
 
 /**
  *
@@ -27,73 +15,11 @@ import { useFlow } from './stackflow';
  */
 
 const BottomNavigation = () => {
-  // SERVER
-  const { categoryList, setCategoryList, toggleCategory } = useCategoryList();
-  // 1. URL & 북마크 Title 입력
-  const {
-    url,
-    title,
-    onChangeUrl,
-    onChangeTitle,
-    handleKeyDown,
-    onDeleteInput,
-    resetAllInputs,
-  } = useInputUrl({});
-
-  // 2. 카테고리 선택
-  const { setSelectedCategoryId, selectedCategoryId } = useSelectCategory({});
-
-  // 3. 공개 범위 선택
-  const { onClickPublishScoped, selectedPublishScoped } =
-    useSelectPublishScoped({});
-
-  // 2. 카테고리 변경
-  const onClickCategory = (id: number) => {
-    // 새로운 카테고리 선택
-    setSelectedCategoryId(id);
-    // 선택된 카테고리 변경
-    setCategoryList(toggleCategory(id));
-  };
-
-  // VALIDATION
-  const isValidateUrl = checkValidateURL(url);
-  const isAllWritten = !!(
-    url &&
-    isValidateUrl &&
-    selectedCategoryId &&
-    selectedPublishScoped
-  );
-
-  const { isOpen, close } = useBottomSheet();
   const onClickAddButton = () => {
     push('BookmarkAddPage', {});
   };
   const { replace, push } = useFlow();
   const { name } = useActivity();
-
-  const { memberId } = useAuthStore();
-  const { mutate: postBookmark } = usePOSTBookmarkMutation({
-    resetAll: {
-      resetAllInputs,
-      resetCategory: () => {
-        setSelectedCategoryId(0);
-        setCategoryList(toggleCategory(0));
-      },
-      resetVisibility: () => onClickPublishScoped('SCOPE_PUBLIC'),
-    },
-    memberId,
-  });
-
-  const onClickSubmitButton = () => {
-    postBookmark({
-      url: checkValidateURL(url) ? checkValidateURL(url) : '',
-      title,
-      categoryId: Number(selectedCategoryId),
-      visibility: selectedPublishScoped,
-      memberId,
-    });
-    close();
-  };
 
   const onClickBottomNavigation = (path: NavigatePathKey) => {
     replace(
@@ -107,30 +33,6 @@ const BottomNavigation = () => {
 
   return (
     <>
-      <BookmarkAddBS isOpen={isOpen} close={close}>
-        <ToastList />
-        <BookmarkAddBS.URLInput
-          url={url}
-          title={title}
-          isValidateUrl={isValidateUrl.length > 0}
-          onChangeUrl={onChangeUrl}
-          onChangeTitle={onChangeTitle}
-          handleKeyDown={handleKeyDown}
-          onDeleteInput={onDeleteInput}
-        />
-        <BookmarkAddBS.SelectCategory
-          categoryList={categoryList}
-          onClickCategory={onClickCategory}
-        />
-        <BookmarkAddBS.PublishScoped
-          selectedPublishScoped={selectedPublishScoped}
-          onClickPublishScoped={onClickPublishScoped}
-        />
-        <BookmarkAddBS.SubmitButton
-          onClick={onClickSubmitButton}
-          isAllWritten={isAllWritten}
-        />
-      </BookmarkAddBS>
       <NavigationWrapper>
         <button onClick={onClickAddButton} css={plusButton}>
           <Icon name="plus" size="m" />
