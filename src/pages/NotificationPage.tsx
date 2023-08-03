644ea9d2c1dfbe2a94c7ee409fb3e799d7f86b01
ww8007 <wshmin1234@gmail.com>
2023-08-03 17:04:00 +0900

    refactor: skeletonWrapper 적용

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -2,15 +2,20 @@ import { Suspense } from 'react';
 import Header from '@/common-ui/Header/Header';
 import NotificationSkeletonItem from '@/notification/ui/NotificationSkeletonItem';
 import Notifications from '@/notification/ui/Notifications';
+import SkeletonWrapper from '@/common-ui/SkeletonWrapper';
 
 const NotificationPage = () => {
   return (
     <>
       <Header title={'🔔 알림 왔어요!'} />
       <Suspense
-        fallback={[1, 2, 3].map((item) => (
-          <NotificationSkeletonItem key={item} />
-        ))}
+        fallback={
+          <SkeletonWrapper>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <NotificationSkeletonItem key={index} />
+            ))}
+          </SkeletonWrapper>
+        }
       >
         <Notifications />
       </Suspense>
