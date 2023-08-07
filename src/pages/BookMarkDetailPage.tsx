82ec1fcecc14405cb795d30f024a85e1ae5406ff
ww8007 <wshmin1234@gmail.com>
2023-08-08 05:51:44 +0900

    fix: 내 포스트인지 확인하는 동작이 제대로 동작하지 않던 오류 수정

--- src/pages/BookMarkDetailPage.tsx
+++ src/pages/BookMarkDetailPage.tsx
@@ -1,62 +1,21 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import Header from '@/common-ui/Header/Header';
 import CommentUploadInput from '@/comment/ui/bookmark/CommentUploadInput';
 import styled from '@emotion/styled';
 import BookmarkArticle from '@/bookmarks/ui/BookmarkArticle';
 import { Suspense } from 'react';
 import CommentList from '@/comment/ui/bookmark/CommentList';
-import TriggerBottomSheet from '@/common-ui/BottomSheet/TriggerBottomSheet';
-import IconButton from '@/common/ui/IconButton';
-import BSConfirmation from '@/common/ui/BSConfirmation';
-import useHandleBookmarkDetailMore from '@/bookmarks/service/hooks/detail/useHandleBookmarkDetailMore';
 import SkeletonBookmarkDetail from '@/bookmarks/ui/Detail/SkeletonBookmarkDetail';
 import SkeletonWrapper from '@/common-ui/SkeletonWrapper';
 import SkeletonCommentList from '@/comment/ui/bookmark/SkeletonCommentList';
+import BookmarkDetailHeader from '@/bookmarks/ui/Detail/BookmarkDetailHeader';
+import Header from '@/common-ui/Header/Header';
 
 const BookMarkDetailPage = () => {
-  const {
-    deleteBookmarkBS,
-    editBookmarkBS,
-    isMyBookmark,
-    closeDeleteBookmarkBS,
-    closeEditBookmarkBS,
-    onClickBackCallback,
-    onClickDeleteBookmark,
-    onClickEditBookmark,
-    openDeleteBookmarkBS,
-    onClickReportBookmark,
-  } = useHandleBookmarkDetailMore();
-
   return (
     <>
-      <Header
-        rightButton={
-          <TriggerBottomSheet>
-            <TriggerBottomSheet.Trigger
-              as={<IconButton onClick={() => {}} name="more" size="s" />}
-            />
-            <TriggerBottomSheet.BottomSheet>
-              {!!isMyBookmark && (
-                <>
-                  <TriggerBottomSheet.Item onClick={onClickEditBookmark}>
-                    수정하기
-                  </TriggerBottomSheet.Item>
-                  <TriggerBottomSheet.Item onClick={openDeleteBookmarkBS}>
-                    삭제하기
-                  </TriggerBottomSheet.Item>
-                </>
-              )}
-              {!isMyBookmark && (
-                <TriggerBottomSheet.Item onClick={onClickReportBookmark}>
-                  신고하기
-                </TriggerBottomSheet.Item>
-              )}
-            </TriggerBottomSheet.BottomSheet>
-          </TriggerBottomSheet>
-        }
-        showBackButton
-        backButtonCallback={onClickBackCallback}
-      />
+      <Suspense fallback={<Header showBackButton />}>
+        <BookmarkDetailHeader />
+      </Suspense>
       <Body>
         {/** 북마크 정보 영역 */}
         <Suspense
@@ -66,11 +25,7 @@ const BookMarkDetailPage = () => {
             </SkeletonWrapper>
           }
         >
-          <BookmarkArticle
-            editBookmarkBS={editBookmarkBS}
-            openEditBookmarkBS={onClickEditBookmark}
-            closeEditBookmarkBS={closeEditBookmarkBS}
-          />
+          <BookmarkArticle />
         </Suspense>
         {/** 댓글 리스트 영역 */}
         <Suspense
@@ -87,15 +42,6 @@ const BookMarkDetailPage = () => {
       <CommentUploadInputBottomBar>
         <CommentUploadInput />
       </CommentUploadInputBottomBar>
-      {/** 북마크 삭제 BS */}
-      <BSConfirmation
-        open={deleteBookmarkBS}
-        onCancel={closeDeleteBookmarkBS}
-        onClose={closeDeleteBookmarkBS}
-        title="정말로 삭제하시겠습니까?"
-        description="삭제된 북마크는 복구할 수 없습니다."
-        onConfirm={onClickDeleteBookmark}
-      />
     </>
   );
 };
