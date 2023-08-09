10a82af9905ae25cabd1e0d9e9f8142dab41f21a
ww8007 <wshmin1234@gmail.com>
2023-08-09 14:35:17 +0900

    fix: 카테고리 저장 후 라우팅 이동 제대로 되지 않던 오류 수정

--- src/category/api/add.ts
+++ src/category/api/add.ts
@@ -3,9 +3,6 @@ import client from '@/common/service/client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { GET_CATEGORY_LIST } from './category';
-import useBookmarkStore from '@/store/bookmark';
-import { navigatePath } from '@/constants/navigatePath';
-import { bookmarkAddPagePaths } from '@/pages/CategoryManagePage';
 
 interface CategoryItem {
   name: string;
@@ -36,30 +33,12 @@ export const usePOSTCategoryMutation = ({ memberId }: POSTCategoryMutation) => {
   const queryClient = useQueryClient();
 
   const router = useNavigate();
-  const { fromPath } = useBookmarkStore();
 
   return useMutation(POSTCategory.API, {
     onSuccess: async () => {
       await queryClient.refetchQueries(GET_CATEGORY_LIST(memberId));
       queryClient.refetchQueries(GET_BOOKMARK_CATEGORY_LIST(memberId));
 
-      if (fromPath === navigatePath.MAIN) {
-        router('/', {
-          preventScrollReset: true,
-          state: {
-            isCategoryAddPage: true,
-          },
-        });
-        return;
-      }
-      if (bookmarkAddPagePaths.some((path) => path.includes(fromPath))) {
-        router(fromPath, {
-          state: {
-            isCategoryAddPage: true,
-          },
-        });
-        return;
-      }
       router(-1);
     },
   });
