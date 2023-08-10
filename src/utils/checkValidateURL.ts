82a584fb4b55c87ae8b2e6b26f436293ad5bb716
ww8007 <wshmin1234@gmail.com>
2023-08-10 19:28:56 +0900

    fix: 북마크 title 파싱 오류 수정

--- src/utils/checkValidateURL.ts
+++ src/utils/checkValidateURL.ts
@@ -1,5 +1,6 @@
 const checkValidateURL = (url: string) => {
-  const regex = /https?:\/\/[^\s]+/g;
+  const regex =
+    /https?:\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;()*\p{Script=Hangul}]+/gu;
 
   const result = url.match(regex);
   return result ? result[0] : '';
