8646533b6ca691619d4db40d59f836cc85416aae
ww8007 <wshmin1234@gmail.com>
2023-08-14 10:19:23 +0900

    feat: 알림 왔을 경우 빨간점 표시 추가 style: 메인 아이콘 변경

--- src/common-ui/BottomNavigation.tsx
+++ src/common-ui/BottomNavigation.tsx
@@ -5,6 +5,9 @@ import styled from '@emotion/styled';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Icon from './assets/Icon';
 import { BOTTOM_NAVIGATION_Z_INDEX } from '@/constants/zIndex';
+import { BsBookFill } from 'react-icons/bs';
+import { useGETNotificationListQuery } from '@/notification/api/notification';
+import useAuthStore from '@/store/auth';
 
 /**
  *
@@ -20,6 +23,11 @@ const BottomNavigation = () => {
   const onClickAddButton = () => {
     router(navigatePath.BOOKMARK_ADD);
   };
+  const { memberId } = useAuthStore();
+  const { data: notificationList } = useGETNotificationListQuery({ memberId });
+  const isCheckNotification = notificationList?.some(
+    (notification) => !notification.isChecked,
+  );
 
   return (
     <>
@@ -27,36 +35,53 @@ const BottomNavigation = () => {
         <button onClick={onClickAddButton} css={plusButton}>
           <Icon name="plus" size="m" />
         </button>
-        <Link to={navigatePath.MAIN} css={iconStyle}>
+        <StyledLink to={navigatePath.MAIN} css={iconStyle}>
           {pathname === `${navigatePath.MAIN}` && (
-            <Icon size="l" name="list-green" />
+            <BsBookFill size={24} color={theme.colors.lightPrimary} />
           )}
-          {pathname !== `${navigatePath.MAIN}` && <Icon size="l" name="list" />}
-        </Link>
-        <Link to="/friend" css={iconStyle}>
+          {pathname !== `${navigatePath.MAIN}` && (
+            <BsBookFill size={24} color={theme.colors.white} />
+          )}
+        </StyledLink>
+        <StyledLink to="/friend" css={iconStyle}>
           {pathname === `${navigatePath.FRIEND}` && (
             <Icon size="l" name="people-green" />
           )}
           {pathname !== `${navigatePath.FRIEND}` && (
             <Icon size="l" name="people" />
           )}
-        </Link>
-        <Link to={navigatePath.NOTIFICATION} css={iconStyle}>
-          {pathname === `${navigatePath.NOTIFICATION}` && (
-            <Icon size="m" name="alarm-green" />
-          )}
-          {pathname !== `${navigatePath.NOTIFICATION}` && (
-            <Icon size="m" name="alarm" />
-          )}
-        </Link>
-        <Link to="/profile" css={iconStyle}>
+        </StyledLink>
+        {!!isCheckNotification && (
+          <NotificationWithRedDot>
+            <StyledLink to="/notification" css={iconStyle}>
+              {pathname === `${navigatePath.NOTIFICATION}` && (
+                <Icon size="m" name="alarm-green" />
+              )}
+              {pathname !== `${navigatePath.NOTIFICATION}` && (
+                <Icon size="m" name="alarm" />
+              )}
+            </StyledLink>
+            <RedDot />
+          </NotificationWithRedDot>
+        )}
+        {!isCheckNotification && (
+          <StyledLink to="/notification" css={iconStyle}>
+            {pathname === `${navigatePath.NOTIFICATION}` && (
+              <Icon size="m" name="alarm-green" />
+            )}
+            {pathname !== `${navigatePath.NOTIFICATION}` && (
+              <Icon size="m" name="alarm" />
+            )}
+          </StyledLink>
+        )}
+        <StyledLink to="/profile" css={iconStyle}>
           {pathname === `${navigatePath.PROFILE}` && (
             <Icon size="m" name="profile-green" />
           )}
           {pathname !== `${navigatePath.PROFILE}` && (
             <Icon size="m" name="profile" />
           )}
-        </Link>
+        </StyledLink>
       </NavigationWrapper>
     </>
   );
@@ -77,6 +102,33 @@ const NavigationWrapper = styled.div`
   background-color: ${theme.colors.grey900};
   z-index: ${BOTTOM_NAVIGATION_Z_INDEX};
   height: 5rem;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+`;
+
+const NotificationWithRedDot = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  height: 3rem;
+`;
+
+const RedDot = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  background-color: ${theme.colors.red};
 `;
 
 const iconStyle = css`
