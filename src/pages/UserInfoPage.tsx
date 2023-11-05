f9925db8d22c61c5cf06f23b9eccd98fadd766b4
ww8007 <wshmin1234@gmail.com>
2023-11-06 02:52:50 +0900

    fix: nickname prefix 로직 삭제 refactor: 합성 컴포넌트 방식으로 변경

--- src/pages/UserInfoPage.tsx
+++ src/pages/UserInfoPage.tsx
@@ -1,121 +1,125 @@
-import UserProfileInfo from '@/user/ui/UserProfileInfo';
+import React, {
+  FormEventHandler,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import useChangeEmoji from '@/common/service/useChangeEmoji';
 import useHandleInput from '@/common/service/useHandleInput';
-import { FormEventHandler, useEffect, useState } from 'react';
 import useAuthStore from '@/store/auth';
 import { usePutUserInfoQuery } from '@/user/api/user';
 import { 특수문자_제거, 특수문자_확인 } from '@/utils/check';
 import useToast from '@/common-ui/Toast/hooks/useToast';
-
-const PREFIX = '-+@*' as const;
+import Header from '@/common-ui/Header/Header';
+import BottomFixedButton from '@/common-ui/BottomFixedButton';
+import UserProfileInfo from '@/user/ui/UserProfileInfo';
 
 interface UserCreatePageProps {
   mode: Mode;
 }
 
 const UserInfoPage = ({ mode }: UserCreatePageProps) => {
+  // FIRST RENDER
   const { userInfo, memberId } = useAuthStore();
   const { fireToast } = useToast();
 
-  const [name, onChangeElementName, onChangeName] = useHandleInput();
-  const [nickname, onChangeElementNickname, onChangeNickname] =
-    useHandleInput();
-
+  // USER INTERACTION
+  // 1. 이름, 닉네임, 이모지 변경
+  const [name, onChangeElementName, setName] = useHandleInput();
+  const [nickname, onChangeElementNickname, setNickname] = useHandleInput();
   const { emoji, isEmojiBSOpen, onChangeEmoji, setEmojiBSOpen, closeEmojiBS } =
     useChangeEmoji();
 
-  // TODO : 이게 최선의 방법인지 고민해보기
   const [initialValues, setInitialValues] = useState({
     name: '',
     nickname: '',
     emoji: '',
   });
-
   const [disabled, setDisabled] = useState(true);
 
-  useEffect(() => {
-    if (userInfo) {
-      const { name, nickname, profileEmoji } = userInfo;
+  useEffect(() => initializeUserInfo(), [userInfo, mode]);
+  useEffect(() => checkFormStatus(), [name, nickname, emoji, initialValues]);
 
-      if (nickname.includes(PREFIX)) onChangeNickname('');
-      else onChangeNickname(nickname);
+  const initializeUserInfo = useCallback(() => {
+    if (!userInfo) return;
 
-      onChangeName(name);
-      onChangeEmoji(profileEmoji);
-      setInitialValues({
-        name,
-        nickname,
-        emoji: profileEmoji,
-      });
-    }
-  }, [userInfo]);
+    const { name, nickname, profileEmoji } = userInfo;
+    if (mode === 'CREATE') setNickname('');
+    if (mode === 'EDIT') setNickname(nickname);
 
-  useEffect(() => {
-    const {
-      name: initialName,
-      nickname: initialNickname,
-      emoji: initialEmoji,
-    } = initialValues;
+    setName(name);
+    onChangeEmoji(profileEmoji);
+    setInitialValues({ name, nickname, emoji: profileEmoji });
+  }, [userInfo, mode]);
 
-    if (nickname.length === 0) {
-      setDisabled(true);
-      return;
-    }
+  // 2. 유저 정보 수정
+  const checkFormStatus = useCallback(() => {
+    const isNicknameEmpty = !nickname;
+    const isNoChange =
+      name === initialValues.name &&
+      nickname === initialValues.nickname &&
+      emoji === initialValues.emoji;
 
-    if (
-      name === initialName &&
-      nickname === initialNickname &&
-      emoji === initialEmoji
-    ) {
-      setDisabled(true);
-      return;
-    }
-    setDisabled(false);
+    setDisabled(isNicknameEmpty || isNoChange);
   }, [name, nickname, emoji, initialValues]);
 
   const { mutate } = usePutUserInfoQuery({ mode, memberId });
 
   const onSubmitUserInfo: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    mutate({
-      putData: {
-        name,
-        nickname,
-        profileEmoji: emoji,
-      },
-      memberId,
-    });
+
+    mutate({ putData: { name, nickname, profileEmoji: emoji }, memberId });
   };
 
-  const onChangeNicknameWithCheck = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (!특수문자_확인(e.target.value)) {
-      fireToast({
-        message: '앗! 닉네임에는 특수문자를 사용할 수 없어요',
-        mode: 'ERROR',
-      });
-      onChangeNickname(특수문자_제거(e.target.value));
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newNickName = e.target.value;
+    // 비어있을 경우 특수문자 확인하지 않음
+    if (!e.target.value.length) {
+      onChangeElementNickname(e);
+      setNickname('');
       return;
     }
-    onChangeElementNickname(e);
+
+    if (특수문자_확인(newNickName)) {
+      onChangeElementNickname(e);
+      return;
+    }
+
+    fireToast({
+      message: '앗! 닉네임에는 특수문자를 사용할 수 없어요',
+      mode: 'ERROR',
+    });
+    setNickname(특수문자_제거(newNickName));
   };
 
   return (
-    <UserProfileInfo
-      emoji={emoji}
-      name={name}
-      nickname={nickname}
-      isEmojiBSOpen={isEmojiBSOpen}
-      buttonDisabled={disabled}
-      mode={mode}
-      onChangeEmoji={onChangeEmoji}
-      setEmojiBSOpen={setEmojiBSOpen}
-      closeEmojiBS={closeEmojiBS}
-      onChangeName={onChangeElementName}
-      onChangeNickname={onChangeNicknameWithCheck}
-      onSubmit={onSubmitUserInfo}
-    />
+    <>
+      <Header showBackButton={mode === 'EDIT'} />
+      <UserProfileInfo onSubmit={onSubmitUserInfo}>
+        <UserProfileInfo.EmojiSelector
+          emoji={emoji}
+          isEmojiBSOpen={isEmojiBSOpen}
+          closeEmojiBS={closeEmojiBS}
+          onChangeEmoji={onChangeEmoji}
+          setEmojiBSOpen={setEmojiBSOpen}
+        />
+        <UserProfileInfo.EditBoxGroup
+          name={name}
+          nickname={nickname}
+          onChangeName={onChangeElementName}
+          onChangeNickname={handleNicknameChange}
+        />
+        {mode === 'EDIT' && <UserProfileInfo.DeleteUserInfo />}
+      </UserProfileInfo>
+      <BottomFixedButton
+        activeButtonColor="lightGreen"
+        buttonColor="buttonGreen"
+        type="submit"
+        disabled={disabled}
+      >
+        {mode === 'CREATE' ? '회원가입' : '수정하기'}
+      </BottomFixedButton>
+    </>
   );
 };
 
