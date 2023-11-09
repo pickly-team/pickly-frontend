860f9727368cd8b6fe9ed2433204810cad5fd205
ww8007 <wshmin1234@gmail.com>
2023-11-10 01:56:25 +0900

    feat: 메인 페이지 UI 추가 및 api 연동

---
+++ src/bookmarks/ui/Main/CategoryReadList.tsx
@@ -0,0 +1,44 @@
+import { useGETBookmarkCategoryStatusQuery } from '@/bookmarks/api/bookmark';
+import useAuthStore from '@/store/auth';
+import useBookmarkStore from '@/store/bookmark';
+import styled from '@emotion/styled';
+import CategoryCard from './CategoryCard';
+
+const CategoryReadList = () => {
+  const { memberId } = useAuthStore();
+  const { data: categoryList } = useGETBookmarkCategoryStatusQuery({
+    memberId,
+  });
+  const { setSelectedCategoryId, setReadOption } = useBookmarkStore();
+
+  const onClickCategory = (id: number) => {
+    setSelectedCategoryId(id);
+    setReadOption('🫣 읽지 않음');
+  };
+
+  return (
+    <Container>
+      {categoryList?.map((category) => (
+        <CategoryCard
+          emoji={category.categoryEmoji}
+          name={category.categoryName}
+          percentage={category.readStatus.readStatusPercentage}
+          readCount={category.readStatus.readCount}
+          totalCount={category.readStatus.total}
+          onClickCategory={() => onClickCategory(category.categoryId)}
+        />
+      ))}
+    </Container>
+  );
+};
+
+export default CategoryReadList;
+
+const Container = styled.div`
+  display: flex;
+  column-gap: 1rem;
+  row-gap: 1rem;
+  width: 100%;
+  flex-wrap: wrap;
+  margin-bottom: 7rem;
+`;
